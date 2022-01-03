e162b912fde64a11cead458fdd16bda7e6df6b08
Lukas Koeller <lukas-koeller@onlinehome.de>
2022-01-03 19:35:58 +0100

    chore(web): add postcss along two postcss plugins
    * postcss-preset-env
    * cssnano

--- apps/web/postcss.config.cjs
+++ apps/web/postcss.config.cjs
@@ -1,7 +1,25 @@
 const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const postcssPresetEnv = require('postcss-preset-env');
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
 
 const config = {
-	plugins: [autoprefixer]
+	plugins: [
+		autoprefixer,
+		postcssPresetEnv({
+			stage: 1,
+			// preserve: true,
+			features: {
+				'dir-pseudo-class': { dir: 'ltr' }
+			}
+		}),
+		!dev &&
+			cssnano({
+				preset: 'default'
+			})
+	]
 };
 
 module.exports = config;
