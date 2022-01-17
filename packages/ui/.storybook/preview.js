8525ec08beadc06eee114039cb039b0cf5fc24bd
Lukas Koeller <acc.koeller@protonmail.com>
2022-01-18 00:05:36 +0100

    chore(ui): add styles (props,...)

--- packages/ui/.storybook/preview.js
+++ packages/ui/.storybook/preview.js
@@ -1,6 +1,9 @@
 import { setCustomElementsManifest } from '@storybook/web-components';
 import customElements from '../custom-elements.json';
 
+import '../src/styles/globals.css';
+import '../src/styles/props.css';
+
 setCustomElementsManifest(customElements);
 
 export const parameters = {
