a9ddf665a3d0ad4433570b8977e0e719b06a49de
Lukas Koeller <acc.koeller@protonmail.com>
2022-02-06 14:02:16 +0100

    chore(web): add new fd-label component

--- apps/web/src/routes/index.svelte
+++ apps/web/src/routes/index.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
+	// import Counter from '$lib/Counter.svelte';
 	import { onMount } from 'svelte';
   onMount(async () => {
     await import('ui');
@@ -15,55 +15,21 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
 	<fd-button>Hallodri Kalodri</fd-button>
 	<fd-gallery>
-		<div>Test 1</div>
-		<div>Test 2</div>
-		<div>Test 3</div>
+		<div class="kpi">
+			<fd-label tag="div">Hourly Rate</fd-label>
+			<span>123 €</span>
+		</div>
 	</fd-gallery>
 </section>
 
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
+<style lang="postcss">
+	.kpi {
+		text-align: center;
 
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
+		& > span {
+			color: red;
+		}
 	}
 </style>
