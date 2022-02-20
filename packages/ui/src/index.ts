57e58dab306ee812e2d6551441862f2d8732bacf
Lukas Koeller <acc.koeller@protonmail.com>
2022-02-20 01:27:11 +0100

    chore(ui): export <fd-section>

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -2,3 +2,4 @@ export * from './fd-button/fd-button';
 export * from './fd-gallery/fd-gallery';
 export * from './fd-gallery-item/fd-gallery-item';
 export * from './fd-label/fd-label';
+export * from './fd-section/fd-section';
