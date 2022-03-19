54fc587526c2fc5c366236ddadf120cb3d7495bf
Lukas Koeller <acc.koeller@protonmail.com>
2022-03-19 19:57:17 +0100

    chore(web): add elements to Header in __layout

--- apps/web/src/routes/__layout.svelte
+++ apps/web/src/routes/__layout.svelte
@@ -1,11 +1,17 @@
 <script lang="ts">
 	import Header from '$lib/header/Header.svelte';
+	import NavItem from '$lib/navitem/NavItem.svelte';
 	import 'ui/src/styles/globals.css';
 	import 'ui/src/styles/props.css';
 	import '../app.css';
 </script>
 
-<Header />
+<Header>
+	<fd-logo />
+	<nav>
+		<NavItem href="/profile">Profile</NavItem>
+	</nav>
+</Header>
 <main>
 	<slot />
 </main>
