b0ce22e2453f86a0456a7ef35c99475c6a61c024
Lukas Koeller <acc.koeller@protonmail.com>
2022-03-23 23:03:49 +0100

    chore(web): <a> wraps icon and span in Header

--- apps/web/src/lib/header/Header.svelte
+++ apps/web/src/lib/header/Header.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	// import { page } from '$app/stores';
 	import { onMount } from 'svelte';
+	import bellSolid from '../../assets/icons/bell-solid.svg?raw';
+	import cogSolid from '../../assets/icons/cog-solid.svg?raw';
+	import paperPlaneSolid from '../../assets/icons/paper-plane-solid.svg?raw';
+	import userCircleSolid from '../../assets/icons/user-circle-solid.svg?raw';
   onMount(async () => {
     await import('ui');
   });
@@ -9,10 +13,22 @@
 <header>
 	<fd-logo />
 	<nav>
-		<a href="/profile">Profile</a>
-		<a href="/messages">Messages</a>
-		<a href="/notifications">Notifications</a>
-		<a href="/settings">Settings</a>
+		<a href="/profile">
+			<fd-icon file={userCircleSolid} size="lg" />
+			<span>Profile</span>
+		</a>
+		<a href="/messages">
+			<fd-icon file={paperPlaneSolid} size="lg" />
+			<span>Messages</span>
+		</a>
+		<a href="/notifications">
+			<fd-icon file={bellSolid} size="lg" />
+			<span>Notifications</span>
+		</a>
+		<a href="/settings">
+			<fd-icon file={cogSolid} size="lg" />
+			<span>Settings</span>
+		</a>
 	</nav>
 </header>
 
@@ -27,5 +43,11 @@
 	nav {
 		display: flex;
 		gap: var(--size-fluid-3);
+	}
+
+	a {
+		display: grid;
+		justify-items: center;
+		text-decoration: none;
 	}
 </style>
