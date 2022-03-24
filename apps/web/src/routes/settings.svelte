87b5caa999538de263e08851ebc26c69862f5748
Lukas Koeller <acc.koeller@protonmail.com>
2022-03-24 23:35:01 +0100

    feat(web): new Settings page

---
+++ apps/web/src/routes/settings.svelte
@@ -0,0 +1,19 @@
+<script context="module">
+	
+</script>
+
+<svelte:head>
+	<title>Settings</title>
+</svelte:head>
+
+<section>
+	<h1>Settings</h1>
+	<p>
+		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
+		following into your command line and following the prompts:
+	</p>
+</section>
+
+<style>
+	
+</style>
