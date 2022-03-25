f34ae3ce993900bdfe8eee746581cc10851438e5
Lukas Koeller <acc.koeller@protonmail.com>
2022-03-26 00:48:18 +0100

    fix(ui): export fd-card

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,5 @@
 export * from './fd-button/fd-button';
+export * from './fd-card/fd-card';
 export * from './fd-gallery/fd-gallery';
 export * from './fd-gallery-item/fd-gallery-item';
 export * from './fd-label/fd-label';
