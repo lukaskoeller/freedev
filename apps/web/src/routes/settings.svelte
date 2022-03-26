30951b6f5dfa7d2cc78832d7b1fbef43fb987b0c
Lukas Koeller <acc.koeller@protonmail.com>
2022-03-26 01:14:05 +0100

    feat(web): add card to settings

--- apps/web/src/routes/settings.svelte
+++ apps/web/src/routes/settings.svelte
@@ -1,5 +1,8 @@
-<script context="module">
-	
+<script lang="ts">
+	import { onMount } from 'svelte';
+		onMount(async () => {
+			await import('ui');
+		});
 </script>
 
 <svelte:head>
@@ -12,8 +15,18 @@
 		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
 		following into your command line and following the prompts:
 	</p>
+	<fd-card class="settings">
+		<fd-section class="separator" fullWidth={true} hasBorder={true}>
+			<fd-input type="email">E-Mail</fd-input>
+		</fd-section>
+	</fd-card>
 </section>
 
-<style>
-	
+<style lang="postcss">
+	.settings::part(card) {
+		display: block;
+		margin-inline: auto;
+		margin-block: var(--global-spacing);
+		inline-size: 50vw;
+	}
 </style>
