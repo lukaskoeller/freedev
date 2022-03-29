28f4f5d1352881aa32e1a5eb3943627d54183f7e
Lukas Koeller <acc.koeller@protonmail.com>
2022-03-29 22:20:20 +0200

    docs(ui): add story for fd-container

---
+++ packages/ui/src/fd-container/fd-container.stories.ts
@@ -0,0 +1,27 @@
+import { Story, Meta } from '@storybook/web-components';
+import './fd-container';
+import '../fd-gallery/fd-gallery';
+import { html } from 'lit-html';
+
+// More on default export: https://storybook.js.org/docs/web-components/writing-stories/introduction#default-export
+export default {
+  title: 'fd-container',
+  // https://github.com/storybookjs/storybook/blob/88e5774dd94888c4d775695acfd74db824c22346/addons/docs/web-components/README.md
+  component: 'fd-container', // which is also found in the `custom-elements.json`
+  // More on argTypes: https://storybook.js.org/docs/web-components/api/argtypes
+} as Meta;
+
+// More on component templates: https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
+const Template: Story = (args) => html`
+  <fd-container>
+    ${args.slot}  
+  </fd-container>
+`;
+
+/**
+ * Some documentation on this story
+ */
+export const Primary = Template.bind({});
+Primary.args = {
+  slot: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+}
