c17776ddfe2a691b6a089e67c29d0914320fa0e9
Lukas Koeller <acc.koeller@protonmail.com>
2022-04-05 22:43:15 +0200

    chore(types): add enum Breakpoints

---
+++ packages/types/src/breakpoints.type.ts
@@ -0,0 +1,7 @@
+export enum Breakpoints {
+  Mobile = '--mobile',
+  LargeMobile = '--large-mobile',
+  Tablet = '--tablet',
+  SmallDesktop = '--small-desktop',
+  LargeDesktop = '--large-desktop',
+}
