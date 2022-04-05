f58caccf33aeaa210acf1207567bff53a0d3b0b5
Lukas Koeller <acc.koeller@protonmail.com>
2022-04-05 22:41:02 +0200

    refactor(web): start with settings item

--- apps/web/src/routes/settings.svelte
+++ apps/web/src/routes/settings.svelte
@@ -16,62 +16,20 @@
 	This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
 	following into your command line and following the prompts:
 </p>
-<section class="settingsWrapper">
+<fd-container class="settingsWrapper">
 	<fd-card class="settings">
-		<fd-section class="separator" fullWidth={true} hasBorder={true}>
-			<div class="sectionSlot">
-				<div class="sectionIcon">
-					<fd-icon size="xl" file={svgAtSolid} />
-				</div>
+		<div class="item">
+			<div class="icon">
+				<fd-icon size="xl" file={svgAtSolid} />
+			</div>
+			<div class="input">
+				<hr>
 				<fd-input type="email">E-Mail</fd-input>
 			</div>
-		</fd-section>
-		<fd-section class="separator" fullWidth={true} hasBorder={true}>
-			<div class="sectionSlot">
-				<div class="sectionIcon">
-					<fd-icon size="xl" file={svgUserCircleSolid} />
-				</div>
-				<fd-input>Name</fd-input>
-			</div>
-		</fd-section>
+		</div>
 	</fd-card>
-</section>
+</fd-container>
 
 <style lang="postcss">
-	.settingsWrapper {
-		inline-size: 100%;
-		display: flex;
-		justify-content: center;
-	}
-	.settings::part(card) {
-		display: grid;
-		gap: var(--size-8);
-
-		margin-inline: auto;
-		margin-block: var(--global-spacing);
-		inline-size: 50vw;
-	}
-
-	.settings fd-section {
-		&:first-child::part(hr) {
-			display: none;
-		}
-	}
-
-	.separator {
-		--fd-section-padding-block: 0;
-	}
-
-	.sectionSlot {
-		display: flex;
-
-		& fd-input {
-			inline-size: 100%;
-		}
-	}
-
-	.sectionIcon {
-		flex-shrink: 0;
-		inline-size: var(--global-spacing);
-	}
+	
 </style>
