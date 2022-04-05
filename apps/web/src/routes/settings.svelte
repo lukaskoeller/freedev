b744ddf999aa45a1cc15e9ee4ea91bed6d41c2c7
Lukas Koeller <acc.koeller@protonmail.com>
2022-04-05 23:06:34 +0200

    refactor(ui): use more simple DOM for settings

--- apps/web/src/routes/settings.svelte
+++ apps/web/src/routes/settings.svelte
@@ -16,20 +16,41 @@
 	This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
 	following into your command line and following the prompts:
 </p>
-<fd-container class="settingsWrapper">
-	<fd-card class="settings">
+<fd-container>
+	<fd-card>
 		<div class="item">
 			<div class="icon">
 				<fd-icon size="xl" file={svgAtSolid} />
 			</div>
 			<div class="input">
-				<hr>
-				<fd-input type="email">E-Mail</fd-input>
+				<fd-hr />
+				<fd-input type="email" placeholder="example@mail.de">E-Mail</fd-input>
 			</div>
 		</div>
 	</fd-card>
 </fd-container>
 
 <style lang="postcss">
-	
+	::part(container) {
+		display: flex;
+		justify-content: center;
+	}
+	::part(card) {
+		inline-size: min(600px, 100%);
+		padding-inline-end: 0;
+	}
+	.item {
+		display: grid;
+		grid-template-columns: auto 1fr;
+		gap: var(--size-4);
+	}
+
+	.icon {
+		padding-block-start: var(--size-3);
+	}
+
+	.input {
+		display: grid;
+		gap: var(--size-3);
+	}
 </style>
