5e9590b1fb23af4db3e6bbbee20b22deaa9a7c8d
Lukas Koeller <acc.koeller@protonmail.com>
2022-04-08 23:47:53 +0200

    chore(web): wrap h1 in fd-container in settings

--- apps/web/src/routes/settings.svelte
+++ apps/web/src/routes/settings.svelte
@@ -12,11 +12,9 @@ import svgAtSolid from 'assets/icons/at-solid.svg?raw';
 	<title>Settings</title>
 </svelte:head>
 
-<h1>Settings</h1>
-<p>
-	This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-	following into your command line and following the prompts:
-</p>
+<fd-container>
+	<h1>Settings</h1>
+</fd-container>
 <fd-container>
 	<fd-card>
 		<fd-stack>
