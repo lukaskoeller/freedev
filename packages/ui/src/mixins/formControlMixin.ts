ad5d364b4ef21d0bfd83c70b8cdd6f8da700ea8c
Lukas Koeller <acc.koeller@protonmail.com>
2022-05-11 23:36:52 +0200

    feat(ui): add onInput for FormControlMixin

--- packages/ui/src/mixins/formControlMixin.ts
+++ packages/ui/src/mixins/formControlMixin.ts
@@ -7,16 +7,46 @@ export declare class FormControlMixinInterface {
   name: string;
   value: string;
   checked: boolean;
+  onInput(event: InputEvent): void;
 }
 
 export const FormControlMixin = 
   <T extends Constructor<LitElement>>(superClass: T) => {
     class FormControl extends superClass {
+      private associatedForm: HTMLFormElement | null = null;
+      private boundOnSubmit = this.onSubmit.bind(this);
   
-    @property({type: String}) name = '';
-    @property({type: String}) value = '';
-    @property({type: Boolean}) checked = false;
+      @property({type: String}) name = '';
+      @property({type: String}) value = '';
+      @property({type: Boolean}) checked = false;
 
-  };
+      onInput(e: InputEvent) {
+        const target = e.target as HTMLInputElement
+        this.value = target.value;
+      }
+
+      connectedCallback(): void {
+          super.connectedCallback();
+          this.associatedForm = this.closest('form');
+          if(this.associatedForm) {
+            this.associatedForm.addEventListener('formdata', this.boundOnSubmit);
+          }
+      }
+
+      disconnectedCallback() {
+        super.disconnectedCallback();
+        if (this.associatedForm) {
+          this.associatedForm.removeEventListener('formdata', this.boundOnSubmit);
+        }
+      }
+
+      private onSubmit(event: FormDataEvent) {
+        // Do validity check here
+        // this.customValidity();
+
+        // @todo Also adds checkbox data even if :not(:checked)
+        event.formData.append(this.name, this.value)
+      }
+    };
   return FormControl as Constructor<FormControlMixinInterface> & T;
 };
