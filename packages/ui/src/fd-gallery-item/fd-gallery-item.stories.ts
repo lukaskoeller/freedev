5ff688803489772e37c7f3212017680dec717fea
Lukas Koeller <acc.koeller@protonmail.com>
2022-05-18 19:19:20 +0200

    style(ui): update fd-gallery-item prop names

--- packages/ui/src/fd-gallery-item/fd-gallery-item.stories.ts
+++ packages/ui/src/fd-gallery-item/fd-gallery-item.stories.ts
@@ -12,7 +12,7 @@ const Template: Story = (args) => html`
   <fd-gallery>
     ${['1','2','3','4'].map((item) => html`
       <fd-gallery-item
-        style="width: var(--size-14); aspect-ratio: 1; background: #fcc2d7; display: grid; place-content: center; font-size: var(--size-6); color: #22b8cf;"
+        style="width: var(--size-14); aspect-ratio: 1; background: var(--color-surface-2); display: grid; place-content: center; font-size: var(--size-6); color: var(--color-primary-base); border-radius: var(--radius-3);"
       >${item}</fd-gallery-item>
     `)}
   </fd-gallery>
