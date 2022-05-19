04fada2a6e1cb75afc8efc733e18bdc3da27c3fd
Lukas Koeller <acc.koeller@protonmail.com>
2022-05-19 22:11:01 +0200

    feat(web): add signup page

---
+++ apps/web/src/routes/signup.svelte
@@ -0,0 +1,23 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+import { onMount } from 'svelte';
+  onMount(async () => {
+    await import('ui');
+  });
+</script>
+
+<svelte:head>
+	<title>Sign Up</title>
+</svelte:head>
+
+<section>
+  <fd-container>
+    <h1>E-Mail</h1>
+    <form>
+      <fd-input name="email" type="email">E-Mail</fd-input>
+    </form>
+  </fd-container>
+</section>
