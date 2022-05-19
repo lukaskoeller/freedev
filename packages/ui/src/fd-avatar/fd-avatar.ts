d9842299c42c792ace78b30f6a6a1e96b9683f86
Lukas Koeller <acc.koeller@protonmail.com>
2022-05-19 21:19:50 +0200

    feat(ui): add fd-avatar

---
+++ packages/ui/src/fd-avatar/fd-avatar.ts
@@ -0,0 +1,88 @@
+import { html, css, LitElement } from 'lit';
+import { customElement, property, query } from 'lit/decorators.js';
+import { Sizes } from 'types/index';
+
+const SizeMap = {
+  [Sizes.Xs]: 'var(--size-6)',
+  [Sizes.Sm]: 'var(--size-7)',
+  [Sizes.Md]: 'var(--size-8)',
+  [Sizes.Lg]: 'var(--size-9)',
+  [Sizes.Xl]: 'var(--size-10)',
+}
+
+// See https://custom-elements-manifest.open-wc.org/analyzer/getting-started/#supported-jsdoc
+// See https://www.npmjs.com/package/@custom-elements-manifest/analyzer
+/**
+ * An Avatar component.
+ *
+ * @slot This element has a slot
+ * @csspart Avatar - Styles the Avatar's div
+ * @cssprop --fd-avatar-background-color - Adjusts the background color
+ * @cssprop --fd-avatar-color - Adjusts the font color
+ * @cssprop --fd-avatar-border-color - Adjusts the border color
+ * @cssprop --fd-avatar-size - Sets the size of the avatar
+ * @cssprop --fd-avatar-border-radius - Sets the border radius. Default is round
+ */
+@customElement('fd-avatar')
+export class Avatar extends LitElement {
+  static styles = css`
+    :host {
+      --fd-avatar-background-color: var(--color-surface-2);
+      --fd-avatar-color: var(--text-1);
+      --fd-avatar-border-color: var(--color-border);
+      --fd-avatar-border-radius: var(--radius-round);
+    }
+
+    div {
+      display: grid;
+      place-items: center;
+      box-sizing: border-box;
+      overflow: hidden;
+      inline-size: var(--fd-avatar-size, var(--size));
+      block-size: var(--fd-avatar-size, var(--size));
+      color: var(--fd-avatar-color, inherit);
+      border: 2px solid var(--fd-avatar-border-color);
+      border-radius: var(--fd-avatar-border-radius);
+      background-color: var(--fd-avatar-background-color);
+    }
+
+    ::slotted(img) {
+      object-fit: cover;
+    }
+  `
+
+  @property({ type: String })
+  name!: string;
+
+  @query('img') private img!: HTMLImageElement|null;
+
+  @property({ type: String })
+  size?: Sizes;
+
+  render() {
+    return html`
+      <div part="avatar" style="--size: ${SizeMap[
+        this.size ?? Sizes.Xs
+      ]};">
+        <slot>
+          ${this.name?.charAt(0) ?? ''}
+        </slot>
+      </div>
+    `
+  }
+
+  async firstUpdated() {
+    // await the async render of unsafeHTML
+    requestAnimationFrame(() => {
+      if (this.name && this.img) {
+        this.img.setAttribute('alt', this.name);
+      }
+    });
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'fd-avatar': Avatar
+  }
+}
