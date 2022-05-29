79b90abf02fff350e183f8f78c637ea4e196b167
Lukas Koeller <acc.koeller@protonmail.com>
2022-05-29 12:22:03 +0200

    feat(web): add new page with basic `<form>`

--- apps/web/src/routes/signup.svelte
+++ apps/web/src/routes/signup.svelte
@@ -18,6 +18,26 @@ import { onMount } from 'svelte';
     <h1>E-Mail</h1>
     <form>
       <fd-input name="email" type="email" label="E-Mail"></fd-input>
+      <fd-input name="password" type="password" label="Password"></fd-input>
+      <fd-button type="submit">Submit</fd-button>
     </form>
+    <output></output>
+    <script>
+      const form = document.querySelector('form');
+      const output = document.querySelector('output');
+
+      form.addEventListener('submit', event => {
+        event.preventDefault();
+        
+        const form = event.target;
+
+        /** Get all of the form data */
+        const formData = new FormData(form);
+        const data = [...formData.entries()];
+        data.forEach((value, key) => data[key] = value);
+        console.log(data);
+        output.innerHTML = JSON.stringify(data, null, 2);
+      });
+    </script>
   </fd-container>
 </section>
