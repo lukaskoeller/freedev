ce8c492ec1f0df86c9920ed54cf1ad363caa1ff4
Lukas Koeller <acc.koeller@protonmail.com>
2022-05-30 23:25:40 +0200

    chore(ui): remove labelStyles from fd-switch
    If label styles should be used within fd-switch, use fd-label as=span within slot

--- packages/ui/src/fd-switch/fd-switch.ts
+++ packages/ui/src/fd-switch/fd-switch.ts
@@ -1,7 +1,6 @@
 import { css, LitElement } from 'lit'
 import { html } from 'lit/static-html.js';
 import { customElement, property } from 'lit/decorators.js';
-import { labelStyles } from '../fd-label/fd-label.style';
 import { FormControlMixin } from '../mixins/formControlMixin';
 
 /**
@@ -25,130 +24,127 @@ import { FormControlMixin } from '../mixins/formControlMixin';
  */
 @customElement('fd-switch')
 export class Switch extends FormControlMixin(LitElement) {
-  static styles = [
-    labelStyles,
-    css`
+  static styles = css`
+    :host {
+      --fd-switch-thumb-size: var(--size-6);
+      --fd-switch-thumb: var(--color-surface-1);
+      
+      --fd-switch-track-size: calc(var(--fd-switch-thumb-size) * 2);
+      --fd-switch-track-padding: calc(var(--size-1) / 2);
+      --fd-switch-track-inactive: var(--color-surface-4);
+      --fd-switch-track-active: var(--color-primary-base);
+    
+      --fd-switch-thumb-color: var(--fd-switch-thumb);
+      --fd-switch-track-color-inactive: var(--fd-switch-track-inactive);
+      --fd-switch-track-color-active: var(--fd-switch-track-active);
+    
+      --fd-switch-isLTR: 1;
+    }
+
+    @media (prefers-color-scheme:dark) {
       :host {
-        --fd-switch-thumb-size: var(--size-6);
-        --fd-switch-thumb: var(--color-surface-1);
-        
-        --fd-switch-track-size: calc(var(--fd-switch-thumb-size) * 2);
-        --fd-switch-track-padding: calc(var(--size-1) / 2);
         --fd-switch-track-inactive: var(--color-surface-4);
-        --fd-switch-track-active: var(--color-primary-base);
+      }
+    }
+
+    label {
+      display: flex;
+      align-items: center;
+      gap: var(--size-4);
+      justify-content: flex-start
+
+      cursor: pointer;
+      user-select: none;
+      -webkit-tap-highlight-color: transparent;
+    }
+
+    input {
+      --fd-switch-thumb-position: 0%;
+      --fd-switch-thumb-transition-duration: .25s;
       
-        --fd-switch-thumb-color: var(--fd-switch-thumb);
-        --fd-switch-track-color-inactive: var(--fd-switch-track-inactive);
-        --fd-switch-track-color-active: var(--fd-switch-track-active);
-      
-        --fd-switch-isLTR: 1;
-      }
+      padding: var(--fd-switch-track-padding);
+      background: var(--fd-switch-track-color-inactive);
+      inline-size: var(--fd-switch-track-size);
+      block-size: var(--fd-switch-thumb-size);
+      border-radius: var(--fd-switch-track-size);
 
-      @media (prefers-color-scheme:dark) {
-        :host {
-          --fd-switch-track-inactive: var(--color-surface-4);
-        }
-      }
+      appearance: none;
+      -webkit-appearance: none;
+      pointer-events: none;
+      touch-action: pan-y;
+      border: none;
+      outline-offset: 5px;
+      box-sizing: content-box;
 
-      label {
-        display: flex;
-        align-items: center;
-        gap: var(--size-4);
-        justify-content: flex-start
+      flex-shrink: 0;
+      display: grid;
+      align-items: center;
+      grid: [track] 1fr / [track] 1fr;
 
-        cursor: pointer;
-        user-select: none;
-        -webkit-tap-highlight-color: transparent;
-      }
+      transition: background-color .25s ease;
+    }
 
-      input {
-        --fd-switch-thumb-position: 0%;
-        --fd-switch-thumb-transition-duration: .25s;
-        
-        padding: var(--fd-switch-track-padding);
-        background: var(--fd-switch-track-color-inactive);
-        inline-size: var(--fd-switch-track-size);
-        block-size: var(--fd-switch-thumb-size);
-        border-radius: var(--fd-switch-track-size);
+    input::before {
+      --fd-switch-highlight-size: 0;
 
-        appearance: none;
-        -webkit-appearance: none;
-        pointer-events: none;
-        touch-action: pan-y;
-        border: none;
-        outline-offset: 5px;
-        box-sizing: content-box;
+      content: "";
+      cursor: pointer;
+      pointer-events: auto;
+      grid-area: track;
+      inline-size: var(--fd-switch-thumb-size);
+      block-size: var(--fd-switch-thumb-size);
+      background: var(--fd-switch-thumb-color);
+      border-radius: 50%;
+      transition: transform .25s ease;
+      transform: translateX(var(--fd-switch-thumb-position));
+    }
 
-        flex-shrink: 0;
-        display: grid;
-        align-items: center;
-        grid: [track] 1fr / [track] 1fr;
-
-        transition: background-color .25s ease;
-      }
-
+    @media (--fd-switch-motionOK) { 
       input::before {
-        --fd-switch-highlight-size: 0;
-
-        content: "";
-        cursor: pointer;
-        pointer-events: auto;
-        grid-area: track;
-        inline-size: var(--fd-switch-thumb-size);
-        block-size: var(--fd-switch-thumb-size);
-        background: var(--fd-switch-thumb-color);
-        border-radius: 50%;
-        transition: transform .25s ease;
-        transform: translateX(var(--fd-switch-thumb-position));
+        transition: 
+          transform var(--fd-switch-thumb-transition-duration) ease;
       }
+    }
 
-      @media (--fd-switch-motionOK) { 
-        input::before {
-          transition: 
-            transform var(--fd-switch-thumb-transition-duration) ease;
-        }
-      }
+    input:not(:disabled):hover::before {
+      --fd-switch-highlight-size: .5rem;
+    }
 
-      input:not(:disabled):hover::before {
-        --fd-switch-highlight-size: .5rem;
-      }
+    input:checked {
+      background: var(--fd-switch-track-color-active);
+      --fd-switch-thumb-position: calc((var(--fd-switch-track-size) - 100%) * var(--fd-switch-isLTR));
+    }
 
-      input:checked {
-        background: var(--fd-switch-track-color-active);
-        --fd-switch-thumb-position: calc((var(--fd-switch-track-size) - 100%) * var(--fd-switch-isLTR));
-      }
+    input:indeterminate {
+      --fd-switch-thumb-position: calc(
+        calc(calc(var(--fd-switch-track-size) / 2) - calc(var(--fd-switch-thumb-size) / 2))
+        * var(--fd-switch-isLTR)
+      );
+    }
 
-      input:indeterminate {
-        --fd-switch-thumb-position: calc(
-          calc(calc(var(--fd-switch-track-size) / 2) - calc(var(--fd-switch-thumb-size) / 2))
-          * var(--fd-switch-isLTR)
-        );
-      }
+    input:disabled {
+      cursor: not-allowed;
+      --fd-switch-thumb-color: transparent;
+    }
 
-      input:disabled {
-        cursor: not-allowed;
-        --fd-switch-thumb-color: transparent;
-      }
+    input:disabled::before {
+      cursor: not-allowed;
+      box-shadow: inset 0 0 0 2px hsl(0 0% 100% / 50%);
+    }
 
+    @media (prefers-color-scheme: dark) {
       input:disabled::before {
-        cursor: not-allowed;
-        box-shadow: inset 0 0 0 2px hsl(0 0% 100% / 50%);
+        box-shadow: inset 0 0 0 2px hsl(0 0% 0% / 50%);
       }
-
-      @media (prefers-color-scheme: dark) {
-        input:disabled::before {
-          box-shadow: inset 0 0 0 2px hsl(0 0% 0% / 50%);
-        }
-      }
-    `
-  ];
+    }
+  `
 
   @property({type: Boolean, reflect: true})
   checked = false;
 
   render() {
     return html`
-      <label for="input" class="label" part="label">
+      <label for="input" part="label">
         <input
           id="input"
           role="switch"
