5ffa4f88a184d3639cd00d68fc604e5b42900672
Lukas Koeller <acc.koeller@protonmail.com>
2022-05-31 22:40:48 +0200

    style(ui): add expand styling for fd-button

--- packages/ui/src/fd-button/fd-button.ts
+++ packages/ui/src/fd-button/fd-button.ts
@@ -17,42 +17,55 @@ export enum ButtonTags {
  * @slot - This element has a slot
  * @csspart button - The button
  * @cssprop {string} --fd-button-background-color - Adjusts the background-color
+ * @cssprop {string} --fd-button-background-color--host - Adjusts the background-color hover state
+ * @cssprop {string} --fd-button-color - Adjusts the text color
+ * @cssprop {string} --fd-button-color--hover - Adjusts the text color hover state
  */
 @customElement('fd-button')
 export class Button extends FormControlMixin(LitElement) {
   static styles = css`
+    :host {
+      --fd-button-background-color: var(--color-link);
+      --fd-button-background-color--hover: var(--color-link-hover);
+      --fd-button-color: var(--color-on-primary);
+      --fd-button-color--hover: var(--color-on-primary);
+      
+      --fd-button-padding-inline: var(--size-6);
+      --fd-button-block-size: var(--size-8);
+      --fd-button-font-size: var(--font-size-1);
+
+      --fd-button-width: fit-content;
+    }
+
     :host([disabled]) {
       pointer-events: none;
-    }  
+    }
+
+    :host([expand]) {
+      --fd-button-width: 100%;
+    }
 
     :is(button, a) {
-      --fd-button-background-color-default: var(--color-link);
-      --fd-button-background-color-default--hover: var(--color-link-hover);
-      --fd-button-color-default: var(--color-on-primary);
-      --fd-button-color-default--hover: var(--color-on-primary);
-      
-      --fd-button-padding-inline-default: var(--size-6);
-      --fd-button-block-size-default: var(--size-8);
-      --fd-button-font-size-default: var(--font-size-1);
-
+      box-sizing: inherit;
       display: inline-flex;
+      inline-size: var(--fd-button-width, fit-content);
       justify-content: center;
       align-items: center;
       text-align: center;
       gap: var(--space-3);      
 
-      min-block-size: var(--fd-button-block-size, var(--fd-button-block-size-default));
-      padding-inline: var(--fd-button-padding-inline, var(--fd-button-padding-inline-default));
+      min-block-size: var(--fd-button-block-size);
+      padding-inline: var(--fd-button-padding-inline);
 
       font-weight: var(--font-weight-6);
-      font-size: var(--fd-button-font-size, var(--fd-button-font-size-default));
-      color: var(--fd-button-color, var(--fd-button-color-default));
+      font-size: var(--fd-button-font-size);
+      color: var(--fd-button-color);
 
       appearance: none;
       border: none;
       text-decoration: none;
 
-      background-color: var(--fd-button-background-color, var(--fd-button-background-color-default));
+      background-color: var(--fd-button-background-color);
       border-radius: var(--radius-round);
       
       touch-action: manipulation;
@@ -60,28 +73,28 @@ export class Button extends FormControlMixin(LitElement) {
     }
     
     :is(button, a):hover {
-      background-color: var(--fg-button-background-color--hover, var(--fd-button-background-color-default--hover));
-      color: var(--fd-button-color--hover, var(--fd-button-color-default--hover));
+      background-color: var(--fd-button-background-color--hover);
+      color: var(--fd-button-color--hover);
     }
     
     :is(button, a)[data-variant="light"] {
-      --fd-button-background-color-default: var(--light-color-base);
-      --fd-button-background-color-default--hover: var(--light-color-base);
-      --fd-button-color-default: var(--primary-color-base);
-      --fd-button-color-default--hover: var(--primary-color-5);
+      --fd-button-background-color: var(--light-color-base);
+      --fd-button-background-color--hover: var(--light-color-base);
+      --fd-button-color: var(--primary-color-base);
+      --fd-button-color--hover: var(--primary-color-5);
     }
     
     :is(button, a)[data-variant="stealth"] {
-      --fd-button-background-color-default: transparent;
-      --fd-button-background-color-default--hover: transparent;
-      --fd-button-color-default: var(--primary-color-5);
-      --fd-button-color-default--hover: var(--primary-color-4);
+      --fd-button-background-color: transparent;
+      --fd-button-background-color--hover: transparent;
+      --fd-button-color: var(--primary-color-5);
+      --fd-button-color--hover: var(--primary-color-4);
     }
     
     :is(button, a)[data-size="small"] {
-      --fd-button-padding-inline-default: var(--size-3);
-      --fd-button-block-size-default: var(--size-7);
-      --fd-button-font-size-default: var(--font-size-0);
+      --fd-button-padding-inline: var(--size-3);
+      --fd-button-block-size: var(--size-7);
+      --fd-button-font-size: var(--font-size-0);
     }
 
     ::slotted(button[slot="proxy"]) {
@@ -96,6 +109,9 @@ export class Button extends FormControlMixin(LitElement) {
 
   @property()
   size: "default" | "small" = "default";
+
+  @property()
+  expand: boolean = false;
 
   @property()
   href?: string;
