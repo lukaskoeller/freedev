5d4991c59c265e43322ec312ee33d0aba98b2483
Lukas Koeller <acc.koeller@protonmail.com>
2022-06-01 22:38:48 +0200

    feat(ui): add new component fd-visually-hidden

---
+++ packages/ui/src/fd-visually-hidden/fd-visually-hidden.ts
@@ -0,0 +1,47 @@
+import { css, LitElement } from 'lit'
+import { html } from 'lit/static-html.js';
+import { customElement, property } from 'lit/decorators.js';
+import '../fd-label/fd-label';
+
+// See https://custom-elements-manifest.open-wc.org/analyzer/getting-started/#supported-jsdoc
+// See https://www.npmjs.com/package/@custom-elements-manifest/analyzer
+/**
+ * Visually hidden is used when an element needs to be available to assistive technologies like screen readers, but be otherwise hidden.
+ *
+ * @slot - The visually hidden content.
+ */
+@customElement('fd-visually-hidden')
+export class VisuallyHidden extends LitElement {
+  static styles = css`
+    :host {
+      all: initial;
+      inset-block-start: 0px;
+      clip: rect(1px, 1px, 1px, 1px) !important;
+      block-size: 1px !important;
+      position: absolute !important;
+      inline-size: 1px !important;
+      border-width: 0px !important;
+      border-style: initial !important;
+      border-color: initial !important;
+      border-image: initial !important;
+      overflow: hidden !important;
+      padding: 0px !important;
+    }
+
+    slot {
+      display: contents;
+    }
+  `
+
+  render() {
+    return html`
+      <slot />
+    `
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'fd-visually-hidden': VisuallyHidden
+  }
+}
