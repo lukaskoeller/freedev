406de43d1a175f5eda39bdf722fe76ff599809ba
Lukas Koeller <acc.koeller@protonmail.com>
2022-06-01 22:38:35 +0200

    docs(ui): add story for fd-visually-hidden

---
+++ packages/ui/src/fd-visually-hidden/fd-visually-hidden.stories.ts
@@ -0,0 +1,23 @@
+import { Story, Meta } from '@storybook/web-components';
+import './fd-visually-hidden';
+import { html } from 'lit-html';
+
+// More on default export: https://storybook.js.org/docs/web-components/writing-stories/introduction#default-export
+export default {
+  title: 'fd-visually-hidden',
+  // https://github.com/storybookjs/storybook/blob/88e5774dd94888c4d775695acfd74db824c22346/addons/docs/web-components/README.md
+  component: 'fd-visually-hidden', // which is also found in the `custom-elements.json`
+  // More on argTypes: https://storybook.js.org/docs/web-components/api/argtypes
+} as Meta;
+
+// More on component templates: https://storybook.js.org/docs/web-components/writing-stories/introduction#using-args
+const Template: Story = (args) => html`
+  <fd-visually-hidden>
+    ${args.slot}  
+  </fd-visually-hidden>
+`;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  slot: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.'
+}
