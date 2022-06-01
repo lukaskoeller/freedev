b52e3f4c2f7d83b4288ef3ed38d852c53cd429ca
Lukas Koeller <acc.koeller@protonmail.com>
2022-06-01 22:39:04 +0200

    feat(ui): introduce size prop for fd-section

--- packages/ui/src/fd-container/fd-container.ts
+++ packages/ui/src/fd-container/fd-container.ts
@@ -1,7 +1,8 @@
 import { css, LitElement } from 'lit'
 import { html } from 'lit/static-html.js';
-import { customElement } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 import '../fd-label/fd-label';
+import { Sizes } from 'types';
 
 // See https://custom-elements-manifest.open-wc.org/analyzer/getting-started/#supported-jsdoc
 // See https://www.npmjs.com/package/@custom-elements-manifest/analyzer
@@ -9,6 +10,8 @@ import '../fd-label/fd-label';
  * General wrapper providing basic inline and block padding
  *
  * @slot - Content of the container
+ * @property {Sizes} size - Sets the width according to given value and disables padding inline
+ * @csspart container - Styles the Container's section
  * @cssprop {string} --fd-container-padding-inline
  * @cssprop {string} --fd-container-padding-inline-start
  * @cssprop {string} --fd-container-padding-inline-end
@@ -19,32 +22,58 @@ export class Container extends LitElement {
   static styles = css`
     :host {
       --fd-container-padding-inline: var(--global-spacing);
-      --fd-container-padding-inline-start: var(--fd-container-padding-inline);
-      --fd-container-padding-inline-end: var(--fd-container-padding-inline);
       --fd-container-padding-block: calc(var(--global-spacing) / 2);
-    }
 
-    :host([data-content]) {
-      --fd-container-inline-size: calc(4 * var(--size-content-1));
-      --fd-container-padding-inline: 0;
-
-      --fd-container-margin-inline: auto;
-      margin-inline: var(--fd-container-margin-inline);
+      --fd-container-inline-size: auto;
+      --fd-container-margin-inline: 0;
     }
 
     section {
       box-sizing: border-box;
       inline-size: min(var(--fd-container-inline-size, fit-content), 100%);
       margin-inline: var(--fd-container-margin-inline);
-      padding-inline-start: var(--fd-container-padding-inline-start);
-      padding-inline-end: var(--fd-container-padding-inline-end);
+      padding-inline-start: var(--fd-container-padding-inline-start, var(--fd-container-padding-inline));
+      padding-inline-end: var(--fd-container-padding-inline-end, var(--fd-container-padding-inline));
       padding-block: var(--fd-container-padding-block);
+    }
+
+    section[data-size="xs"] {
+      --fd-container-inline-size: calc(1 * var(--size-15));
+      --fd-container-padding-inline: 0px;
+      --fd-container-margin-inline: auto;
+    }
+
+    section[data-size="sm"] {
+      --fd-container-inline-size: calc(1.5 * var(--size-15));
+      --fd-container-padding-inline: 0px;
+      --fd-container-margin-inline: auto;
+    }
+
+    section[data-size="md"] {
+      --fd-container-inline-size: calc(2 * var(--size-15));
+      --fd-container-padding-inline: 0px;
+      --fd-container-margin-inline: auto;
+    }
+
+    section[data-size="lg"] {
+      --fd-container-inline-size: calc(3 * var(--size-15));
+      --fd-container-padding-inline: 0px;
+      --fd-container-margin-inline: auto;
+    }
+
+    section[data-size="xl"] {
+      --fd-container-inline-size: calc(4 * var(--size-15));
+      --fd-container-padding-inline: 0px;
+      --fd-container-margin-inline: auto;
     }
   `
 
+  @property({type: String})
+  size?: Sizes;
+
   render() {
     return html`
-      <section part="container">
+      <section part="container" data-size=${this.size}>
         <slot />
       </section>
     `
