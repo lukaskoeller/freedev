2f1897fc3824ec211f6620be6faf4127ddf18bb5
Lukas Koeller <acc.koeller@protonmail.com>
2022-06-03 00:53:40 +0200

    feat(ui): add loading icon and prop to fd-button

--- packages/ui/src/fd-button/fd-button.ts
+++ packages/ui/src/fd-button/fd-button.ts
@@ -4,6 +4,8 @@ import { html, literal } from 'lit/static-html.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { FormControlMixin } from '../mixins/formControlMixin';
 import { Size, Status } from 'types';
+import '../fd-icon/fd-icon';
+import svgCircleNotchSolid from 'assets/icons/circle-notch-solid.svg?raw';
 
 export enum ButtonTags {
   Button = 'button',
@@ -34,6 +36,7 @@ export class Button extends FormControlMixin(LitElement) {
       --fd-button-padding-inline: var(--size-6);
       --fd-button-block-size: var(--size-8);
       --fd-button-font-size: var(--font-size-1);
+      --fd-button-line-height: var(--font-lineheight-5);
 
       --fd-button-width: fit-content;
     }
@@ -47,12 +50,12 @@ export class Button extends FormControlMixin(LitElement) {
     }
 
     :is(button, a) {
+      position: relative;
       box-sizing: inherit;
-      display: inline-flex;
       inline-size: var(--fd-button-width, fit-content);
+      display: inline-flex;
       justify-content: center;
       align-items: center;
-      text-align: center;
       gap: var(--space-3);      
 
       min-block-size: var(--fd-button-block-size);
@@ -60,6 +63,8 @@ export class Button extends FormControlMixin(LitElement) {
 
       font-weight: var(--font-weight-6);
       font-size: var(--fd-button-font-size);
+      line-height: var(--fd-button-line-height);
+      text-align: center;
       color: var(--fd-button-color);
 
       appearance: none;
@@ -68,6 +73,7 @@ export class Button extends FormControlMixin(LitElement) {
 
       background-color: var(--fd-button-background-color);
       border-radius: var(--radius-round);
+      overflow: hidden;
       
       touch-action: manipulation;
       cursor: pointer;
@@ -98,10 +104,48 @@ export class Button extends FormControlMixin(LitElement) {
       --fd-button-font-size: var(--font-size-0);
     }
 
+    :is(button, a)[data-status="loading"] > [name="status"] {
+      --fd-icon-size: 1.75em;
+      display: inline-flex;
+      justify-content: center;
+      align-items: center;
+      position: absolute;
+      content: "";
+      top: 0;
+      left: 0;
+      inline-size: 100%;
+      block-size: 100%;
+      background-color: var(--fd-button-background-color);
+    }
+
+    @keyframes spin {
+      to {
+        transform: rotate(1turn)
+      }
+    }
+
+    :is(button, a)[data-status="loading"] > [name="status"] > fd-icon {
+      animation: var(--animation-spin);
+      animation-duration: 1.1s;
+    }
+
     ::slotted(button[slot="proxy"]) {
       display: none;
     }
   `
+
+  statusIcon(status: Status | undefined) {
+    switch (status) {
+      case Status.Loading:
+        return html`
+          <fd-icon
+            file=${svgCircleNotchSolid}
+            size="inline"
+          />`;
+      default:
+        return null // render nothing
+    }
+  }
 
   private isLink: boolean = false;
 
@@ -164,6 +208,8 @@ export class Button extends FormControlMixin(LitElement) {
         part="button"
         data-variant=${this.variant}
         data-size=${this.size}
+        data-status=${this.status}
+        ?aria-busy=${this.status === Status.Loading}
         name=${ifDefined(!this.isLink ? this.name : undefined)}
         value=${ifDefined(!this.isLink ? this.value : undefined)}
         type=${ifDefined(!this.isLink ? this.type : undefined)}
@@ -174,6 +220,9 @@ export class Button extends FormControlMixin(LitElement) {
         @click=${this.onClick}
       >
         <slot></slot>
+        <slot name="status">
+          ${this.statusIcon(this.status)}
+        </slot>
       </${this.getTag()}>
     `
   }
