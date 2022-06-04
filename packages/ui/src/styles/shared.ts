4b7da9516a4783a45d2fbb55039b539641535ceb
Lukas Koeller <acc.koeller@protonmail.com>
2022-06-05 00:01:29 +0200

    feat(ui): add shared styles for caption and error

---
+++ packages/ui/src/styles/shared.ts
@@ -0,0 +1,15 @@
+import { css } from "lit";
+
+export const errorStyles = css`
+  .fd-error {
+    margin-block-start: var(--size-1);
+    color: var(--color-status-error);
+  }
+`;
+
+export const captionStyles = css`
+  .fd-caption {
+    font-size: var(--font-size-0);
+    line-height: var(--font-lineheight-1);
+  }
+`;
