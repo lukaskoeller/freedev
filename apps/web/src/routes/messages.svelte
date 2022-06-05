cab6727134713e235e1afba5354d197a74d6a42b
Lukas Koeller <acc.koeller@protonmail.com>
2022-06-05 23:29:03 +0200

    chore(web): add pages to avoid 404
    Leads to build error

---
+++ apps/web/src/routes/messages.svelte
@@ -0,0 +1,21 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<script lang="ts">
+import { onMount } from 'svelte';
+  onMount(async () => {
+    await import('ui');
+  });
+</script>
+
+<svelte:head>
+	<title>Messages</title>
+</svelte:head>
+
+<section>
+  <h1>Messages</h1>
+  <fd-container>
+    <fd-button href="/signup">Sign Up</fd-button>
+  </fd-container>
+</section>
