b9bc886fbb5470ffde0d37b531414c784132593f
Lukas Koeller <acc.koeller@protonmail.com>
2022-06-14 23:59:02 +0200

    chore(infrastructure): add apigateway and WIP lambda

--- packages/infrastructure/index.ts
+++ packages/infrastructure/index.ts
@@ -1,6 +1,8 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
+import * as apigateway from "@pulumi/aws-apigateway";
+import api from './api.json';
 
 // // Create an AWS resource (S3 Bucket)
 // const bucket = new aws.s3.Bucket("my-bucket");
@@ -36,3 +38,39 @@ const pool = new aws.cognito.UserPool('appPool', {
 
 // @todo Create swagger-api with api gateway
 // @see https://github.com/pulumi/examples/tree/master/aws-apigateway-ts-routes
+
+// Docs on enabling a cognito user pool in aws api gateway
+// https://docs.aws.amazon.com/apigateway/latest/developerguide/apigateway-enable-cognito-user-pool.html
+
+// Define whole API using swagger (OpenAPI)
+const swaggerAPI = new apigateway.RestAPI("swagger-api", {
+  swaggerString: JSON.stringify(api),
+});
+
+// Configure IAM so that the AWS Lambda can be run.
+const handleApiTestRole = new aws.iam.Role("handleApiTestRole", {
+  assumeRolePolicy: {
+     Version: "2012-10-17",
+     Statement: [{
+        Action: "sts:AssumeRole",
+        Principal: {
+           Service: "lambda.amazonaws.com",
+        },
+        Effect: "Allow",
+        Sid: "",
+     }],
+  },
+});
+new aws.iam.RolePolicyAttachment("zipTpsReportsFuncRoleAttach", {
+  role: handleApiTestRole,
+  policyArn: aws.iam.ManagedPolicies.AWSLambdaExecute,
+});
+
+const handleApiTest = new aws.lambda.Function("handleApiTest", {
+  // Upload the code for our Lambda from the "./app" directory:
+  code: new pulumi.asset.AssetArchive({
+     ".": new pulumi.asset.FileArchive("./app"),
+  }),
+  runtime: "nodejs12.x",
+  role: handleApiTestRole.arn,
+})
