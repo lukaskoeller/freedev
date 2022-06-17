98e8c53b4b5a038ea6ba172f850a0c5d7b40bd76
Lukas Koeller <acc.koeller@protonmail.com>
2022-06-17 08:27:59 +0200

    chore(infrastructure): rm handleApiTest.ts

--- packages/infrastructure/src/lambdas/handleApiTest.ts
+++
@@ -1,8 +0,0 @@
-'use strict';
-console.log('handleApiTest: Loading function');
-
-exports.handler = (event, context, callback) => {
-  console.log({ event, context });
-  
-  callback(null, `handleApiTest: ${JSON.stringify(event)}`);
-}
