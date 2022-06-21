247ea69fee968b75e1141473d1a97f8098b14575
Lukas Koeller <acc.koeller@protonmail.com>
2022-06-22 00:56:48 +0200

    chore(infrastructure): adjust lambda code

--- packages/infrastructure/src/lambdas/index.js
+++ packages/infrastructure/src/lambdas/index.js
@@ -1,7 +1,11 @@
 exports.handler =  async function(event, context) {
-  console.log("EVENT: \n" + JSON.stringify(event, null, 2))
+  console.log("EVENT: \n" + JSON.stringify(event, null, 2));
   return {
       statusCode: 200,
-      body: `Hello freedev! ${JSON.stringify(event, null, 2)}`
+      body: `Hello freedev! ${JSON.stringify(event, null, 2)}`,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+      },
   };
 }
