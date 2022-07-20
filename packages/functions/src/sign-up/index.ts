820848def43f7ebb1809e9e158e10de83a46e463
Lukas Koeller <acc.koeller@protonmail.com>
2022-07-21 00:22:41 +0200

    chore(functions): logging and add constant

--- packages/functions/src/sign-up/index.ts
+++ packages/functions/src/sign-up/index.ts
@@ -1,28 +1,30 @@
 import { APIGatewayEvent, Context } from "aws-lambda";
 import { RestExceptionNoBody } from "errors";
 import { CognitoIdentityProviderClient, SignUpCommand } from "@aws-sdk/client-cognito-identity-provider";
+// import { userPoolClient } from "infrastructure";
 
 /**
  * AWS Region
  * @see https://www.pulumi.com/registry/packages/aws/api-docs/getregion/
  */
  const REGION = 'eu-central-1';
+ const USER_POOL_CLIENT_ID = '2nn02h56mp8aa9ckj89b2m9mi1';
 
 export type SignupBody = {
   email: string;
   password: string;
 }
 
-async (event: APIGatewayEvent, context: Context) => {
+export const handler = async (event: APIGatewayEvent, context: Context) => {
   // const user = new User()
-  console.log('LOG:LOG:LOG:LOG', {
+  console.log('!!!LOG!!!', {
     event,
     context,
   });
   if (!event?.body) {
     throw RestExceptionNoBody
   }
-  console.log('BODY!!!', event?.body);
+  console.log('!!!BODY!!!', event?.body);
   
   const body: SignupBody = JSON.parse(event?.body);
   const { email, password } = body;
@@ -31,7 +33,7 @@ async (event: APIGatewayEvent, context: Context) => {
   const client = new CognitoIdentityProviderClient({ region: REGION });
 
   const params = {
-    ClientId: 'xyz', // userPoolClient.id
+    ClientId: USER_POOL_CLIENT_ID, // @todo any possibility to use 'userPoolClient.id'?
     Username: email,
     Password: password,
   };
@@ -40,11 +42,16 @@ async (event: APIGatewayEvent, context: Context) => {
   
   // async/await.
   try {
+    console.log('TRY:CLIENT_SEND');
     const data = await client.send(command);
+    console.log('DATA: ', data);
+    
     // process data.
   } catch (error) {
+    console.log('!!!ERROR!!!');
     // error handling.
   } finally {
+    console.log('!!!FINALLY!!!');
     // finally.
   }
   
