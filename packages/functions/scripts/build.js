6e98a44159b2503c52f67f8671bae5ceacff0545
Lukas Koeller <acc.koeller@protonmail.com>
2022-07-21 22:52:26 +0200

    chore(functions): set correct build settings

--- packages/functions/scripts/build.js
+++ packages/functions/scripts/build.js
@@ -1,5 +1,10 @@
 require('esbuild').build({
   entryPoints: ['./src/sign-up'],
   bundle: true,
+  minify: true,
+  platform: 'node',
+  target: 'node16.16.0',
   outfile: './build/sign-up/index.js',
+  format: 'cjs',
+
 }).catch(() => process.exit(1))
