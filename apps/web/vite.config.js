ae4b185833de8e77b3f1fbb6a2712fdeae2bed1e
Lukas Koeller <acc.koeller@protonmail.com>
2022-07-24 21:55:04 +0200

    chore(web): add vite config sveltekit plugin

---
+++ apps/web/vite.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+import { sveltekit } from '@sveltejs/kit/vite';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+  server: {
+    port: 3000,
+  },
+  plugins: [sveltekit()]
+};
+
+export default config;
