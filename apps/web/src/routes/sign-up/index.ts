727b4dff7cee415c108797d8e8700653ae8eff70
Lukas Koeller <acc.koeller@protonmail.com>
2022-08-16 22:05:29 +0200

    chore(web): rename route to sign-up

---
+++ apps/web/src/routes/sign-up/index.ts
@@ -0,0 +1,20 @@
+import type { RequestHandler } from "@sveltejs/kit";
+import { api } from "./_api";
+
+export const PUT: RequestHandler = async (event) => {
+  console.log('PUT');
+  
+  const form = await event.request.formData();
+
+  console.log('form', form);
+  
+  const response = await api('PUT', 'sign-up', {
+    email: form.get('email'),
+    password: form.get('password'),
+  });
+  const body = await response.json();
+  console.log('RESPONSE', response);
+  console.log('BODY', body);
+  
+  return body;
+};
