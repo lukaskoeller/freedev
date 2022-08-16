8c028bdaf0838bb310e4d47411da38a77c7fa1f8
Lukas Koeller <acc.koeller@protonmail.com>
2022-08-16 22:05:55 +0200

    chore(functions): build script to handle directories

--- packages/functions/scripts/build.js
+++ packages/functions/scripts/build.js
@@ -1,10 +1,20 @@
-require('esbuild').build({
-  entryPoints: ['./src/sign-up'],
-  bundle: true,
-  minify: true,
-  platform: 'node',
-  target: 'node16.16.0',
-  outfile: './build/sign-up/index.js',
-  format: 'cjs',
+const glob = require('tiny-glob');
+const { build } = require('esbuild');
 
-}).catch(() => process.exit(1))
+(async () => {
+  let entryPoints = await glob('./src/*/*.{ts}');
+  try {
+    await build({
+      entryPoints,
+      bundle: true,
+      minify: true,
+      platform: 'node',
+      target: 'node16.16.0',
+      outdir: './build',
+      format: 'cjs',
+  
+    });
+  } catch (error) {
+    process.exit(1); 
+  }
+})();
