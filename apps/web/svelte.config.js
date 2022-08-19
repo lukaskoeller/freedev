066b00be2b122eff3149b08d9d0a2bc5d35919da
Lukas Koeller <acc.koeller@protonmail.com>
2022-08-19 22:48:05 +0200

    chore(web): no prerendering

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -16,13 +16,13 @@ const config = {
       // default options are shown
       pages: 'build',
       assets: 'build',
-      fallback: null,
+      fallback: '404.html',
       precompress: false
     }),
 
     prerender: {
       // This can be false if you're using a fallback (i.e. SPA mode)
-      default: true
+      default: false
     },
 
 		methodOverride: {
