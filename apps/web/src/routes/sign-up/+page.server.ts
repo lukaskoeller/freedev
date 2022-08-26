50dbf8b839713217706adc68537fa2e4fb261f58
Lukas Koeller <acc.koeller@protonmail.com>
2022-08-26 23:09:44 +0200

    chore(web): migrate return
    @url https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292699

--- apps/web/src/routes/sign-up/+page.server.ts
+++ apps/web/src/routes/sign-up/+page.server.ts
@@ -15,7 +15,4 @@ export const PUT: Action = async (event) => {
   const body = await response.json();
   console.log('RESPONSE', response);
   console.log('BODY', body);
-  
-  throw new Error("@migration task: Migrate this return statement (https://github.com/sveltejs/kit/discussions/5774#discussioncomment-3292699)");
-  return body;
 };
