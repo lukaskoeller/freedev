96766ea0c7fbce41880771355efd62e33162e895
Lukas Koeller <acc.koeller@protonmail.com>
2022-09-22 23:07:11 +0200

    feat(web): add Stack component

---
+++ apps/web/src/lib/stack/Stack.svelte
@@ -0,0 +1,14 @@
+<div>
+  <slot />
+</div>
+
+<style lang="postcss">
+  div {
+    --fd-stack-gap: var(--size-3);
+    --fd-stack-justify-items: stretch;
+
+    display: grid;
+    gap: var(--fd-stack-gap);
+    justify-items: var(--fd-stack-justify-items);
+  }
+</style>
