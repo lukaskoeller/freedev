300a87de3d1ed830b38655caefb5c4e0ca56bfef
Lukas Koeller <acc.koeller@protonmail.com>
2022-09-22 19:51:47 +0200

    refactor(infrastructure): add createApi with endpoints

--- packages/infrastructure/src/functions/index.ts
+++ packages/infrastructure/src/functions/index.ts
@@ -1,2 +1,22 @@
-export * from './root';
-export * from './sign-up';
+import { api, HttpMethod, Microservice } from '../common/models';
+
+export const createApi = () => {
+  /**
+   * @todo Iterate through openapi.json and create new Microservice.
+   */
+  const signUpEndpoint = new Microservice({
+    name: 'sign-up',
+    path: '/sign-up',
+    httpMethod: HttpMethod.PUT,
+    api,
+  });
+
+  /**
+   * Root of website (homepage). Path is '/' or 'https://freedev.app'.
+   */
+  const rootEndpoint = new Microservice({
+    name: 'default',
+    path: '$default',
+    api,
+  });
+}
