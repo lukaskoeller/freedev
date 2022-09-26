88d864cb64501eb369962c561e12c34a4cea8f20
Lukas Koeller <acc.koeller@protonmail.com>
2022-09-26 08:17:27 +0200

    feat(web): add SearchBar component

---
+++ apps/web/src/lib/searchbar/SearchBar.svelte
@@ -0,0 +1,47 @@
+<div class="bar">
+  <form>
+    <div class="search">
+      <input type="search" placeholder="z.B. React Frontend Engineer" name="search" id="search">
+      <button type="submit">Search</button>
+    </div>
+  </form>
+</div>
+
+<style lang="postcss">
+  .bar {
+    padding: var(--size-fluid-1);
+    border-radius: var(--radius-round);
+    border: var(--border-size-1) solid var(--color-border);
+    background-color: var(--color-surface-1);
+    box-shadow: var(--shadow-3);
+  }
+
+  form {
+    display: block;
+    inline-size: 100%;
+    block-size: 100%;
+  }
+
+  .search {
+    inline-size: 100%;
+    display: grid;
+    grid-template-columns: 1fr auto;
+    gap: var(--size-3);
+  }
+
+  input {
+    block-size: min(var(--size-fluid-5), 100%);
+    padding-inline-start: calc(var(--size-fluid-5) / 2);
+    border-radius: var(--radius-round);
+    background: transparent;
+    font-size: var(--font-size-fluid-1);
+  }
+
+  button {
+    aspect-ratio: 1;
+    block-size: 100%;
+    border-radius: var(--radius-round);
+    background-color: var(--color-primary-base);
+    color: var(--color-on-primary);
+  }
+</style>
