f23e2d09f3708de416a46eec0f36157e706bccd8
Lukas Koeller <acc.koeller@protonmail.com>
2022-09-26 08:17:47 +0200

    chore(web): add wrapper for bg svg

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -4,25 +4,33 @@
 	import 'ui/src/styles/globals.css';
 	import 'ui/src/styles/props.css';
 	import '../app.css';
+	import bgSvg from '../assets/svg/freedev-bg.svg?url';
+	import bgLgSvg from '../assets/svg/freedev-bg-large.svg?url';
 	onMount(async () => {
     await import('ui');
   });
 </script>
 
-<Header />
-<div class="logoWrapper">
-	<a href="/">
-		<fd-logo />
-	</a>
-</div>
-<main>
-	<slot />
-</main>
-<footer>
-	<!-- <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p> -->
-</footer>
+<section style:background={`url('${bgLgSvg}') center 20% no-repeat`}>
+	<Header />
+	<div class="logoWrapper">
+		<a href="/">
+			<fd-logo />
+		</a>
+	</div>
+	<main>
+		<slot />
+	</main>
+	<footer>
+		<!-- <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p> -->
+	</footer>
+</section>
 
 <style lang="postcss">
+
+	section {
+		background-repeat: no-repeat;
+	}
 	.logoWrapper {
 		display: grid;
 		place-items: center;
