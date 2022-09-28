c181efe8d585550fbc7b00eeee94506d4f8547d9
Lukas Koeller <acc.koeller@protonmail.com>
2022-09-28 21:31:11 +0200

    chore(web): use fixed number of key info in ProfileCard

--- apps/web/src/lib/profilecard/ProfileCard.svelte
+++ apps/web/src/lib/profilecard/ProfileCard.svelte
@@ -2,7 +2,7 @@
 	import Stack from "$lib/stack/Stack.svelte";
 </script>
 
-<fd-card class="card" style="--fd-card-padding-inline: 0px;">
+<fd-card class="card" style="--fd-card-padding-inline: var(--size-3);">
   <Stack --fd-stack-gap="var(--size-7)">
     <div class="header">
       <div class="avatar"></div>
@@ -15,28 +15,18 @@
       </div>
     </div>
     <div class="key-info">
-      <fd-gallery style="--fd-gallery-gap: var(--size-3);">
-        <fd-gallery-item>
-          <fd-label as="div">Hourly Rate</fd-label>
-				  <span>123 €</span>
-        </fd-gallery-item>
-        <fd-gallery-item>
-          <fd-label as="div">Available From</fd-label>
-          <span>Feb 12</span>
-        </fd-gallery-item>
-        <fd-gallery-item>
-          <fd-label as="div">Projects</fd-label>
-				  <span>42</span>
-        </fd-gallery-item>
-        <fd-gallery-item>
-          <fd-label as="div">Happiness</fd-label>
-				  <span>4.3 (27)</span>
-        </fd-gallery-item>
-        <fd-gallery-item>
-          <fd-label as="div">Capacity</fd-label>
+      <div>
+        <fd-label as="div">Hourly Rate</fd-label>
+        <span>123 €</span>
+      </div>
+      <div>
+        <fd-label as="div">Available From</fd-label>
+        <span>Feb 12</span>
+      </div>
+      <div>
+        <fd-label as="div">Capacity</fd-label>
 				  <span>30h/week</span>
-        </fd-gallery-item>
-      </fd-gallery>
+      </div>
     </div>
   </Stack>
 </fd-card>
@@ -49,7 +39,6 @@
   .header {
     display: flex;
     gap: var(--size-7);
-    padding-inline: var(--size-3);
   }
 
   .avatar {
@@ -72,6 +61,9 @@
   }
 
   .key-info {
+    display: flex;
+    justify-content: space-around;
+    gap: var(--size-3);
     inline-size: 100%;
     min-width: 0;
   }
