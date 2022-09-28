e3b9dadbe16ef314ff045166a8f2bae1b1dc3110
Lukas Koeller <acc.koeller@protonmail.com>
2022-09-28 20:43:40 +0200

    refactor(ui): get rid of proxy css props in fd-label.style

--- packages/ui/src/fd-label/fd-label.style.ts
+++ packages/ui/src/fd-label/fd-label.style.ts
@@ -2,16 +2,16 @@ import { css } from "lit";
 
 export const labelStyles = css`
   :host {
-    --fd-label-color-default: var(--primary-color-5);
-    --fd-label-text-transform-default: uppercase;
-    --fd-label-font-weight-default: var(--font-weight-6);
-    --fd-label-font-size-default: var(--font-size-1);
+    --fd-label-color: var(--primary-color-5);
+    --fd-label-text-transform: uppercase;
+    --fd-label-font-weight: var(--font-weight-6);
+    --fd-label-font-size: var(--font-size-1);
   }
 
   .label {
-    font-weight: var(--fd-label-font-weight, var(--fd-label-font-weight-default));
-    font-size: var(--fd-label-font-size, var(--fd-label-font-size-default));
-    text-transform: var(--fd-label-text-transform, var(--fd-label-text-transform-default));
-    color: var(--fd-label-color, var(--fd-label-color-default));
+    font-weight: var(--fd-label-font-weight);
+    font-size: var(--fd-label-font-size);
+    text-transform: var(--fd-label-text-transform);
+    color: var(--fd-label-color);
   }
 `;
