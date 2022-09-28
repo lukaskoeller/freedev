a22d66d8b134e5c9f094ed8d0d47d2f4d49c83d2
Lukas Koeller <acc.koeller@protonmail.com>
2022-09-28 09:47:21 +0200

    chore(web): adjust Stack so that prop styling works
    https://svelte.dev/docs#template-syntax-component-directives---style-props

--- apps/web/src/lib/stack/Stack.svelte
+++ apps/web/src/lib/stack/Stack.svelte
@@ -4,11 +4,8 @@
 
 <style lang="postcss">
   div {
-    --fd-stack-gap: var(--size-3);
-    --fd-stack-justify-items: stretch;
-
     display: grid;
-    gap: var(--fd-stack-gap);
-    justify-items: var(--fd-stack-justify-items);
+    gap: var(--fd-stack-gap, var(--size-3));
+    justify-items: var(--fd-stack-justify-items, stretch);
   }
 </style>
