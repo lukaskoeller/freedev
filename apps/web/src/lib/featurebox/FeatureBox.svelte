5ed2804239db41ac70398603daa90fa0b4ec08c7
Lukas Koeller <acc.koeller@protonmail.com>
2022-09-30 13:15:43 +0200

    feat(web): add FeatureBox component

---
+++ apps/web/src/lib/featurebox/FeatureBox.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  export const heading: string = undefined;
+</script>
+
+<div class="fd-stack">
+  {#if $$slots.figure}
+    <div class="figure">
+      <slot name="figure"></slot>
+    </div>
+  {/if}
+  <h4>{heading}</h4>
+  <p>
+    <slot></slot>
+  </p>
+</div>
+<style lang="postcss">
+  .fd-stack {
+    grid-column: span var(--fd-feature-box-grid-column-span);
+  }
+</style>
