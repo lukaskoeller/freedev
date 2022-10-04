ee1f931bd0bda3ba52ad8524bce6eeab0adec8bd
Lukas Koeller <acc.koeller@protonmail.com>
2022-10-05 00:43:38 +0200

    feat(web): add FeatureSection component

---
+++ apps/web/src/lib/featuresection/FeatureSection.svelte
@@ -0,0 +1,14 @@
+<section class="fd-ram-grid">
+  <slot></slot>
+</section>
+
+<style lang="postcss">
+  .fd-ram-grid {
+    --fd-ram-grid-min-width: var(--size-14);
+    --fd-ram-grid-gap: var(--size-6);
+
+    @media (--tablet) {
+      --fd-ram-grid-gap: var(--size-9);
+    }
+  }
+</style>
