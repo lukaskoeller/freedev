11d1aff888a597201b69707e0fc361efe9b72f08
Lukas Koeller <acc.koeller@protonmail.com>
2022-10-06 23:39:29 +0200

    chore(infrastructure): mv api and apiStage

--- packages/infrastructure/src/common/models/api.ts
+++ packages/infrastructure/src/common/models/api.ts
@@ -144,36 +144,3 @@ export class Microservice {
     });
   }
 }
-
-export const api = new aws.apigatewayv2.Api("httpApiGateway", {
-  protocolType: "HTTP",
-});
-
-export const apiStage = new aws.apigatewayv2.Stage(`${pulumi.getStack()}-api-stage`, {
-  apiId: api.id,
-  // @see https://stackoverflow.com/questions/61027859/conflictexception-stage-already-exist-from-aws-api-gateway-deployment-with-stag
-  name: pulumi.getStack(), // @todo rework?
-  autoDeploy: true,
-  // deploymentId: `${pulumi.getStack()}-api-deployment-id`,
-  // @todo integrate into microservice!?
-  // routeSettings: [
-  //   {
-  //     routeKey: this.apigatewayv2Route.routeKey,
-  //     throttlingBurstLimit: 5000,
-  //     throttlingRateLimit: 10000,
-  //   },
-  // ],
-  // autoDeploy: true,
-});
-
-// const apiDeployment = new aws.apigatewayv2.Deployment("freedev-api-deployment", {
-//   apiId: api.id,
-//   description: "freedev api deployment",
-// }, {
-//   dependsOn: [
-//     signup.apigatewayv2Route,
-//     root.apigatewayv2Route,
-//   ]
-// });
-
-export const endpoint = pulumi.interpolate`${api.apiEndpoint}/${apiStage.name}`;
--- packages/infrastructure/src/functions/index.ts
+++ packages/infrastructure/src/functions/index.ts
@@ -1,6 +1,12 @@
-import { api, HttpMethod, Microservice } from '../common/models';
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+import { HttpMethod, Microservice } from '../common/models';
 
 export const createApi = () => {
+  const api = new aws.apigatewayv2.Api("httpApiGateway", {
+    protocolType: "HTTP",
+  });
+
   /**
    * @todo Iterate through openapi.json and create new Microservice.
    */
@@ -19,4 +25,33 @@ export const createApi = () => {
     path: '$default',
     api,
   });
+
+  const apiStage = new aws.apigatewayv2.Stage(`${pulumi.getStack()}-api-stage`, {
+    apiId: api.id,
+    // @see https://stackoverflow.com/questions/61027859/conflictexception-stage-already-exist-from-aws-api-gateway-deployment-with-stag
+    name: pulumi.getStack(), // @todo rework?
+    autoDeploy: true,
+    // deploymentId: `${pulumi.getStack()}-api-deployment-id`,
+    // @todo integrate into microservice!?
+    // routeSettings: [
+    //   {
+    //     routeKey: this.apigatewayv2Route.routeKey,
+    //     throttlingBurstLimit: 5000,
+    //     throttlingRateLimit: 10000,
+    //   },
+    // ],
+    // autoDeploy: true,
+  });
+
+  // const apiDeployment = new aws.apigatewayv2.Deployment("freedev-api-deployment", {
+  //   apiId: api.id,
+  //   description: "freedev api deployment",
+  // }, {
+  //   dependsOn: [
+  //     signup.apigatewayv2Route,
+  //     root.apigatewayv2Route,
+  //   ]
+  // });
+
+  const endpoint = pulumi.interpolate`${api.apiEndpoint}/${apiStage.name}`;
 }
