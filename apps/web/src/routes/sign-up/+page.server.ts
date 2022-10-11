782a63ad1fcb9a5b8b5bb48666a45f8e69a27139
Lukas Koeller <acc.koeller@protonmail.com>
2022-10-11 22:31:40 +0200

    refactor(web): migrate to actions for form handling

--- apps/web/src/routes/sign-up/+page.server.ts
+++ apps/web/src/routes/sign-up/+page.server.ts
@@ -1,18 +1,19 @@
-import type { Action } from "@sveltejs/kit";
+import type { Actions } from "@sveltejs/kit";
 import { api } from "./_api";
 
-export const PUT: Action = async (event) => {
-  console.log('PUT');
-  
-  const form = await event.request.formData();
+export const actions: Actions = {
+  signup: async ({ request, cookies }) => {
+    console.log('request', request);
+    
+    const form = await request.formData();
 
-  console.log('form', form);
-  
-  const response = await api('PUT', 'sign-up', {
-    email: form.get('email'),
-    password: form.get('password'),
-  });
-  const body = await response.json();
-  console.log('RESPONSE', response);
-  console.log('BODY', body);
+    const response = await api('PUT', 'sign-up', {
+      email: form.get('email'),
+      password: form.get('password'),
+    });
+    const body = await response.json();
+    console.log('BODY', body);
+    
+    return body;
+  },
 };
