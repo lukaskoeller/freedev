b9628a53561a9b1bbaf48e5561a7fcb9cde7337c
Lukas Koeller <acc.koeller@protonmail.com>
2022-10-14 08:33:29 +0200

    chore(web): set search params and insert them to form

--- apps/web/src/routes/sign-up/+page.svelte
+++ apps/web/src/routes/sign-up/+page.svelte
@@ -4,6 +4,9 @@
   import type { ActionData } from './$types';
   import { onMount } from 'svelte';
 	import { Size } from 'types';
+  import { page } from '$app/stores';
+	import SplitInput from '$lib/splitinput/SplitInput.svelte';
+	import { bind } from 'svelte/internal';
 
   onMount(async () => {
     await import('ui');
@@ -15,7 +18,9 @@
   // }
 
   export let form: ActionData;
-  
+
+  $: username = $page.url.searchParams.get('username');
+  $: email = $page.url.searchParams.get('email');
 </script>
 
 <svelte:head>
@@ -25,37 +30,25 @@
 <section>
   <Container>
     <Container size={Size.Xs}>
-      {#if form?.statusCode !== 200}
-        <div class="fd-stack">
-          <h1>Sign Up</h1>
-          <form
-            action="?/signup"
-            method="POST"
-            use:enhance
-          >
-            <div class="fd-stack">
-              <fd-input name="email" type="email" label="E-Mail" required></fd-input>
-              <fd-input name="password" type="password" label="Password"></fd-input>
-              <fd-switch name="terms">I agree with the terms & conditions.</fd-switch>
-              <fd-button type="submit" expand>Submit</fd-button>
-              {#if form?.message}
-                <p>{form?.message}</p>
-              {/if}
-            </div>
-          </form>
-        </div>
-      {:else}
-        <div class="fd-stack">
-          <h1>Wohooo!</h1>
-          <h2>E-Mail bestätigen</h2>
-          <p>
-            Wir haben dir einen Link an <strong>{form?.email}</strong> geschickt.
-            Mit einem Klick bestätigst du deine Email.
-          </p>
-          <p>Nichts bekommen? Schau mal in deinen Spam-Ordner.</p>
-          <fd-button>Erneut senden</fd-button>
-        </div>
-      {/if}
+      <div class="fd-stack">
+        <h1>Sign Up</h1>
+        <form
+          action="?/signUp"
+          method="POST"
+          use:enhance
+        >
+          <div class="fd-stack">
+            <fd-input name="email" type="email" label="E-Mail" required></fd-input>
+            <fd-input name="password" type="password" label="Password"></fd-input>
+            <fd-switch name="terms">I agree with the terms & conditions.</fd-switch>
+            <fd-button type="submit" expand>Submit</fd-button>
+            {#if form?.message}
+              <p>{form?.message}</p>
+            {/if}
+            <output>{JSON.stringify(form)}</output>
+          </div>
+        </form>
+      </div>
     </Container>
   </Container>
   <Container>
