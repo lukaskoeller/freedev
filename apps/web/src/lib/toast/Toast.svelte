053b13cb92a4bbca9880213fa952cf0d1129f7ee
Lukas Koeller <acc.koeller@protonmail.com>
2022-10-16 23:33:53 +0200

    feat(web): add Toast component

---
+++ apps/web/src/lib/toast/Toast.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  export let message: string;
+</script>
+
+<div class="toast fd-card">
+  {#if $$slots.before}
+    <div class="before">
+      <slot name="before"></slot>
+    </div>
+  {/if}
+  <div class="main">
+    <strong>{message}</strong>
+    {#if $$slots.body}
+      <div class="body">
+        <slot name="body"></slot>
+      </div>
+    {/if}
+  </div>
+  <div class="after">
+    <slot name="after">
+      x
+    </slot>
+  </div>
+</div>
+
+<style lang="postcss">
+  .toast {
+    --fd-card-padding-inline: var(--size-4);
+    --fd-card-padding-block: var(--size-4);
+
+    display: flex;
+    gap: var(--size-4);
+  }
+
+  .main {
+    display: grid;
+    gap: var(--size-1);
+  }
+</style>
