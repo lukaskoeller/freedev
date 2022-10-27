e4fbfb2cce9a3e29c6c61388b613df7eb905887e
Lukas Koeller <acc.koeller@protonmail.com>
2022-10-28 00:46:30 +0200

    feat(web): improve from handling for sign-up

--- apps/web/src/routes/confirm-sign-up/+page.svelte
+++ apps/web/src/routes/confirm-sign-up/+page.svelte
@@ -6,12 +6,16 @@
 	import { applyAction, enhance } from "$app/forms";
 	import SplitInput from "$lib/splitinput/SplitInput.svelte";
 	import { onMount } from "svelte";
+	import { invalidateAll } from "$app/navigation";
+	import { notifications } from "$lib/toast/notifications";
+
 
   onMount(async () => {
     await import('ui');
   });
 
   export let form: ActionData;
+  let isSubmitting: boolean = false;
 
   $: username = $page.url.searchParams.get('username');
   $: email = $page.url.searchParams.get('email');
@@ -33,11 +37,38 @@
         </p>
         <form
           method="POST"
-          use:enhance={() => {
-            return async ({ result, update }) => {
-              applyAction(result);
-              if (result.type === 'success') return;
-              update();
+          use:enhance={(props) => {
+            const { form } = props;
+            
+            isSubmitting = true;
+            return async (args) => {
+              console.log({ args });
+              const { result, update } = args;
+              switch (result.type) {
+                case 'success':
+                  notifications.success('Created new product');
+                  invalidateAll();
+                  break;
+                case 'error':
+                  notifications.error('Something went wrong when trying to create a new product');
+                  // await applyAction(result);
+                  form.reset();
+                  // invalidateAll();
+                  break;
+                case 'invalid':
+                  notifications.warning(result.data.body.data.message);
+                  // form.reset();
+                  invalidateAll();
+                  await applyAction(result);
+                  break;
+                default:
+                  notifications.info('This is a default message');
+                  form.reset();
+                  invalidateAll();
+                  await applyAction(result);
+                  update();
+              }
+              isSubmitting = false;
             };
           }}
         >
