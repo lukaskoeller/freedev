9307e2a204b69ace48369bf19a5b15eadee91ec8
Lukas Koeller <acc.koeller@protonmail.com>
2022-10-30 22:01:44 +0100

    feat(web): add notification container to layout

--- apps/web/src/routes/+layout.svelte
+++ apps/web/src/routes/+layout.svelte
@@ -5,6 +5,9 @@
 	import 'ui/src/styles/props.css';
 	import '../app.css';
 	import bgLgSvg from '$lib/assets/svg/freedev-bg-large.svg?url';
+	import { notifications } from '$lib/toast/notifications';
+	import Toast from '$lib/toast/Toast.svelte';
+	import { flip } from 'svelte/animate';
 	onMount(async () => {
     await import('ui');
   });
@@ -23,6 +26,20 @@
 	<footer>
 		<!-- <p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p> -->
 	</footer>
+	<div
+		class="toastContainer"
+		role="region"
+		aria-live="polite"
+	>
+	{#each $notifications as notification (notification.id)}
+		<div class="item" animate:flip>
+			<Toast
+				type={notification.type}
+				message={notification.message}
+			></Toast>
+		</div>
+	{/each}
+	</div>
 </section>
 
 <style lang="postcss">
@@ -44,5 +61,24 @@
 		@media (--tablet) {
 			display: none;
 		}
+	}
+
+	.toastContainer {
+		position: fixed;
+		padding: var(--size-3);
+    z-index: var(--layer-5);
+    pointer-events: none;
+    display: flex;
+		flex-direction: column;
+		align-items: center;
+    gap: var(--size-3);
+    margin: 0px auto;
+    bottom: env(safe-area-inset-bottom, 0px);
+    right: env(safe-area-inset-right, 0px);
+    left: env(safe-area-inset-left, 0px);	
+	}
+
+	.item {
+		display: contents;
 	}
 </style>
