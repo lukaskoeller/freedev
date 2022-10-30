6af5f58a81d2237d9d656bc230aa2fee5bb3941a
Lukas Koeller <acc.koeller@protonmail.com>
2022-10-30 22:01:26 +0100

    chore(web): imrpove form handling for sign-up

--- apps/web/src/routes/sign-up/+page.svelte
+++ apps/web/src/routes/sign-up/+page.svelte
@@ -1,23 +1,20 @@
 <script lang="ts">
   import Container from '$lib/container/Container.svelte';
-  import { enhance } from '$app/forms';
+  import { applyAction, enhance } from '$app/forms';
   import type { ActionData } from './$types';
   import { onMount } from 'svelte';
 	import { Size } from 'types';
   import { page } from '$app/stores';
-	import SplitInput from '$lib/splitinput/SplitInput.svelte';
-	import { bind } from 'svelte/internal';
+	import { notifications } from '$lib/toast/notifications';
+	import { validate, type FieldErrors } from './_validations';
+	import { invalidateAll } from '$app/navigation';
 
   onMount(async () => {
     await import('ui');
   });
-  // import { browser } from '$app/env';
- 
-  // if (browser) {
-  //   import('ui');
-  // }
 
-  export let form: ActionData;
+  let formErrors: undefined | FieldErrors;
+  let isSubmitting: boolean = false;
 
   $: username = $page.url.searchParams.get('username');
   $: email = $page.url.searchParams.get('email');
@@ -35,17 +32,81 @@
         <form
           action="?/signUp"
           method="POST"
-          use:enhance
+          use:enhance={(props) => {
+            isSubmitting = true;
+            const { form, data, cancel } = props;
+              for (const [key, value] of data.entries()) {
+                console.log(`${key}: ${value}`); 
+              }
+            form.dispatchEvent(new CustomEvent('submitting'));
+            const email = data.get('email');
+            const password = data.get('password');
+
+            const formData = {
+              email,
+              password,
+            };
+            
+            const { isValid, fields } = validate(formData);
+
+            if (!isValid) {
+              cancel();
+              formErrors = fields;
+              isSubmitting = false;
+              return;
+            }
+
+            return async (args) => {
+              console.log({ args });
+              const { result, update } = args;
+              switch (result.type) {
+                case 'success':
+                  notifications.success('Welcome on board 👋');
+                  invalidateAll();
+                  break;
+                case 'error':
+                  notifications.error('Something went wrong when signing you up 😢');
+                  // await applyAction(result);
+                  form.reset();
+                  // invalidateAll();
+                  break;
+                case 'invalid':
+                  notifications.warning(result.data.message);
+                  // form.reset();
+                  invalidateAll();
+                  await applyAction(result);
+                  break;
+                default:
+                  form.reset();
+                  invalidateAll();
+                  await applyAction(result);
+                  update();
+              }
+              isSubmitting = false;
+            }
+          }}
         >
           <div class="fd-stack">
-            <fd-input name="email" type="email" label="E-Mail" required></fd-input>
-            <fd-input name="password" type="password" label="Password"></fd-input>
+            <fd-input
+              name="email"
+              type="email"
+              label="E-Mail"
+              required
+              error={formErrors?.get('email')}
+            ></fd-input>
+            <fd-input
+              name="password"
+              type="password"
+              label="Password"
+              error={formErrors?.get('password')}
+            ></fd-input>
             <fd-switch name="terms">I agree with the terms & conditions.</fd-switch>
-            <fd-button type="submit" expand>Submit</fd-button>
-            {#if form?.message}
-              <p>{form?.message}</p>
-            {/if}
-            <output>{JSON.stringify(form)}</output>
+            <fd-button
+              type="submit"
+              status={isSubmitting ? 'loading' : undefined}
+              expand
+            >Submit</fd-button>
+            isSubmitting: {isSubmitting}
           </div>
         </form>
       </div>
@@ -53,6 +114,27 @@
   </Container>
   <Container>
     <Container size={Size.Xs}>
+      <!-- <Toast message="Successfully signed up!">
+        <div slot="before">
+          <Icon
+            size={Size.Md}
+            --fd-icon-fill="var(--color-status-success)"
+          >{@html checkCircleSvg}</Icon>
+        </div>
+        <p slot="body" class="fd-info-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia, et fugit, commodi quis sequi blanditiis autem.</p>
+      </Toast>
+      <Toast message="Successfully signed up!">
+        <p slot="body" class="fd-info-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quia, et fugit, commodi quis sequi blanditiis autem.</p>
+      </Toast>
+      <Toast message="Successfully signed up!"></Toast>
+      <Toast message="Successfully signed up!">
+        <div slot="before">
+          <Icon
+            size={Size.Md}
+            --fd-icon-fill="var(--color-status-success)"
+          >{@html checkCircleSvg}</Icon>
+        </div>
+      </Toast> -->
       <div class="fd-stack">
         <h1>What's your name?</h1>
         <fd-input label="Name" name="name" />
