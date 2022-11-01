ce97283cc6073b80f17bf16c76e7f14604d193a2
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-01 22:09:57 +0100

    refactor(functions): use constants from common

--- packages/functions/src/sign-in/index.ts
+++ packages/functions/src/sign-in/index.ts
@@ -7,13 +7,7 @@ import {
   InitiateAuthCommandInput,
 } from "@aws-sdk/client-cognito-identity-provider";
 import { ApiErrorResponse, ApiResponse, InternalErrorException, NoBodyException } from "../common/utils";
-
-/**
- * AWS Region
- * @see https://www.pulumi.com/registry/packages/aws/api-docs/getregion/
- */
-const REGION = 'eu-central-1';
-const USER_POOL_CLIENT_ID = '6b3h1osi597ifm5at8qcsavg5d';
+import { AWS_REGION, USER_POOL_CLIENT_ID } from "../common/constants";
 
 export type SignInBody = {
   email: string;
@@ -34,7 +28,7 @@ export const handler = async (event: APIGatewayProxyEventV2, context: Context) =
   }
   
   // a client can be shared by different commands.
-  const client = new CognitoIdentityProviderClient({ region: REGION });
+  const client = new CognitoIdentityProviderClient({ region: AWS_REGION });
 
   const params: InitiateAuthCommandInput = {
     ClientId: USER_POOL_CLIENT_ID, // @todo any possibility to use 'userPoolClient.id'?
