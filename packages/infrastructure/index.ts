9b9eee25d283d81b3444add91885f29ac229fbaf
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-01 07:52:34 +0100

    chore(infrastructure): pass userPoolClientId to createApi

--- packages/infrastructure/index.ts
+++ packages/infrastructure/index.ts
@@ -10,12 +10,12 @@ import { getDomainAndSubdomain } from "./src/common/utils";
  * Cognito User Pool
  * Includes email configuration
  */
-export const { userPoolClientId, userPoolEndpoint } =  createUserPoolAndClient();
+export const { userPoolEndpoint, userPoolClientId } =  createUserPoolAndClient();
 
 /**
  * API Gateway v2
  */
-createApi({ userPoolEndpoint });
+createApi({ userPoolClientId, userPoolEndpoint });
 
 /**
  * US East Region Provider
