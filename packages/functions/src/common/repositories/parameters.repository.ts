9353089c790696c900d2ab7c0b88bdbee439c6f1
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-01 11:57:26 +0100

    feat(functions): add parameters file as first draft

---
+++ packages/functions/src/common/repositories/parameters.repository.ts
@@ -0,0 +1,42 @@
+// https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/clients/client-ssm/classes/getparametercommand.html#input
+
+import { SSMClient, GetParameterCommand } from "@aws-sdk/client-ssm";
+import { INTERNAL_ERROR_MESSAGE } from "errors";
+import { AWS_REGION } from "../constants";
+import { ApiErrorResponse, InternalErrorException } from "../utils";
+
+/**
+ * Get information about a single parameter by specifying the parameter name.
+ * @param {GetParameterCommandInput['Name']} name The name of the parameter you want to query.
+ * To query by parameter label, use `"Name": "name:label"`. To query by parameter
+ * version, use `"Name": "name:version"`.
+ */
+export const getParameterByName = async (name: string) => {
+  if (!name) {
+    throw new ApiErrorResponse({
+      statusCode: 500,
+      body: {
+        status: 500,
+        message: INTERNAL_ERROR_MESSAGE,
+        debugMessage: 'A `name` is missing as argument.',
+      }
+    })
+  }
+  const config = {
+    region: AWS_REGION, 
+  }
+  
+  const input = {
+    Name: name,
+  }
+  try {
+    const client = new SSMClient(config);
+    const command = new GetParameterCommand(input);
+    const response = await client.send(command);
+  
+    return response;
+ 
+  } catch (error) {
+    throw InternalErrorException;
+  }
+};
