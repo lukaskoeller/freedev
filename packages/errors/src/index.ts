e9b35c584fc39dfe85606c7a3d9f2794dfe3d590
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-01 11:56:36 +0100

    feat(errors): add internal error message

--- packages/errors/src/index.ts
+++ packages/errors/src/index.ts
@@ -1,2 +1,3 @@
 export const DEFAULT_ERROR_MESSAGE = 'An error occured. Please try again later.';
 export const REQUIRED_ERROR_MESSAGE = 'This field is required.';
+export const INTERNAL_ERROR_MESSAGE = 'An internal error occured on our side. We could not sign you in. Please try connecting again. If the issue keeps happening, contact Customer Care.';
