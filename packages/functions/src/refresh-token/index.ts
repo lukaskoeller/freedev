f6f4a9ce9f519cbcfc399f790b0a7e806e98b4b5
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-06 20:34:41 +0100

    feat(functions): add refresh-token lambda

---
+++ packages/functions/src/refresh-token/index.ts
@@ -0,0 +1,73 @@
+import { APIGatewayProxyEventV2, Context } from "aws-lambda";
+import { DEFAULT_ERROR_MESSAGE } from "errors";
+import {
+  AuthFlowType,
+  CognitoIdentityProviderClient,
+  InitiateAuthCommand,
+  InitiateAuthCommandInput,
+} from "@aws-sdk/client-cognito-identity-provider";
+import { ApiErrorResponse, ApiResponse, InternalErrorException, NoBodyException } from "../common/utils";
+import { AWS_REGION, USER_POOL_CLIENT_ID } from "../common/constants";
+
+export type RefreshTokenBody = {
+  refreshToken: string;
+  deviceKey: string;
+}
+
+export const handler = async (event: APIGatewayProxyEventV2, context: Context) => {
+  if (!event?.body) {
+    throw NoBodyException;
+  }
+
+  const body: RefreshTokenBody = JSON.parse(event?.body);
+  const { refreshToken, deviceKey } = body;
+  const ipAddress = event?.['requestContext']?.['http']?.['sourceIp'];
+
+  if (!ipAddress) {
+    throw InternalErrorException;
+  }
+  
+  // a client can be shared by different commands.
+  const client = new CognitoIdentityProviderClient({ region: AWS_REGION });
+
+  const params: InitiateAuthCommandInput = {
+    ClientId: USER_POOL_CLIENT_ID, // @todo any possibility to use 'userPoolClient.id'?
+    AuthFlow: AuthFlowType.REFRESH_TOKEN,
+    UserContextData: {
+      IpAddress: ipAddress,
+      // EncodedData
+    },
+    AuthParameters: {
+      REFRESH_TOKEN: refreshToken,
+      // SECRET_HASH https://docs.aws.amazon.com/cognito-user-identity-pools/latest/APIReference/API_InitiateAuth.html#API_InitiateAuth_RequestParameters
+      DEVICE_KEY: deviceKey,
+    },
+  };
+
+  const command = new InitiateAuthCommand(params);
+  try {
+    const data = await client.send(command);
+    console.log(data);
+
+    return new ApiResponse({
+      statusCode: 200,
+      body: data,
+    })
+  } catch (error) {
+    // @todo add throw Error as return
+    console.log('!!!ERROR!!!', error);
+
+    return new ApiErrorResponse({
+      statusCode: error?.statusCode ?? 500,
+      body: {
+        status: error?.statusCode ?? 500,
+        message: error?.message
+          ? `${error?.message}${error?.code ?? ` (${error?.code}`}`
+          : DEFAULT_ERROR_MESSAGE,
+        debugMessage: 'Some text to debug the error',
+      }
+    });
+  } finally {
+    // finally.
+  }
+};
