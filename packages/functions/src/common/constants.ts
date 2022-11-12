6a627c9b4a774371b063b130a4e3d06eded35dc6
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-12 01:06:08 +0100

    feat(functions): add DYNAMO_DB_TABLE_NAME

--- packages/functions/src/common/constants.ts
+++ packages/functions/src/common/constants.ts
@@ -7,3 +7,7 @@ export const AWS_REGION = 'eu-central-1';
  * Cognito User Pool Client ID
  */
 export const USER_POOL_CLIENT_ID = '6b3h1osi597ifm5at8qcsavg5d';
+/**
+ * Main DynamoDB Table Name
+ */
+export const DYNAMO_DB_TABLE_NAME = 'main-dynamodb-table-7994558';
