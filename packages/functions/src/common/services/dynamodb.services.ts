8136ecfc882a6168f204b27dcd4a45674cac7464
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-17 23:35:24 +0100

    chore(functions): add read method and indexName param

--- packages/functions/src/common/services/dynamodb.services.ts
+++ packages/functions/src/common/services/dynamodb.services.ts
@@ -1,34 +1,59 @@
-import { AttributeValue, DynamoDBClient, InternalServerError, PutItemCommand } from "@aws-sdk/client-dynamodb";
+import { AttributeValue, DynamoDBClient, GetItemCommand, GetItemCommandInput, InternalServerError, PutItemCommand, PutItemCommandInput } from "@aws-sdk/client-dynamodb";
 import { marshall } from "@aws-sdk/util-dynamodb";
 import { AWS_REGION } from "../constants";
 import { ApiErrorResponse, InternalErrorException } from "../utils";
 
 export type DynamoDBServiceParams = {
   tableName: string;
+  indexName?: string;
 };
 
 export class DynamoDBService {
   tableName: string;
+  indexName?: string;
 
   constructor(parameters: DynamoDBServiceParams) {
-    this.tableName = parameters.tableName; 
+    this.tableName = parameters.tableName;
+    this.indexName = parameters.indexName;
   }
 
   // Create an Amazon DynamoDB service client object.
   client = new DynamoDBClient({ region: AWS_REGION });
 
   async create(item: Record<string, unknown>) {
-    const params = {
+    const params: PutItemCommandInput = {
       TableName: this.tableName,
       Item: marshall(item),
+      // Avoid replace items with create
+      ConditionExpression: 'attribute_not_exists(pk)',
     };
 
     try {
       const command = new PutItemCommand(params);
       const response = await this.client.send(command);
-      console.log("Success - item added or updated", response);
+      console.log("Success - item added", response);
     } catch (error) {
       console.log("PutItemCommand Error", error.stack);
+      throw InternalServerError;
+    }
+  }
+
+  async read(item: { pk: string, sk: string }) {
+    const params: GetItemCommandInput = {
+      TableName: this.tableName,
+      ...this.indexName ? { IndexName: this.indexName } : {},
+      Key: marshall({
+        pk: item.pk,
+        sk: item.sk,
+      }),
+    };
+
+    try {
+      const command = new GetItemCommand(params);
+      const response = await this.client.send(command);
+      console.log("Success - item read", response);
+    } catch (error) {
+      console.log("GetItemCommand Error", error.stack);
       throw InternalServerError;
     }
   }
