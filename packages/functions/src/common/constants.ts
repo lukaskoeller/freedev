4cfa33df291bccdf63c619348dd3c5145ca19a92
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-17 23:34:58 +0100

    chore(functions): add constants for dynamo and user pool

--- packages/functions/src/common/constants.ts
+++ packages/functions/src/common/constants.ts
@@ -8,6 +8,20 @@ export const AWS_REGION = 'eu-central-1';
  */
 export const USER_POOL_CLIENT_ID = '6b3h1osi597ifm5at8qcsavg5d';
 /**
+ * Cognito User Pool ID
+ */
+export const USER_POOL_ID = 'eu-central-1_RhkLD4h08';
+/**
  * Main DynamoDB Table Name
  */
-export const DYNAMO_DB_TABLE_NAME = 'main-dynamodb-table-7994558';
+export const DYNAMO_DB_TABLE_NAME = 'main-dynamodb-table-bfad842';
+export const DYNAMO_DB_TABLE_NAME_INDEX = 'handle-index';
+export const DYNAMO_DB_TABLE_ARN = 'arn:aws:dynamodb:eu-central-1:865039251033:table/main-dynamodb-table-bfad842';
+
+/**
+ * DynamoDB Prefixes to determine data type
+ * in a single table application.
+ */
+export enum DBPrefix {
+  User = 'USER#',
+}
