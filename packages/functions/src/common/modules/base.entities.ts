d60e9b940c0ca0d03d82d1b425d687b06429b313
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-19 12:54:33 +0100

    feat(functions): add base entity

---
+++ packages/functions/src/common/modules/base.entities.ts
@@ -0,0 +1,15 @@
+export type ItemKey = {
+  pk: string,
+  sk: string,
+};
+
+export abstract class Item {
+  abstract get pk(): string
+  abstract get sk(): string
+  public keys(): ItemKey {
+    return {
+      pk: this.pk,
+      sk: this.sk,
+    };
+  }
+}
