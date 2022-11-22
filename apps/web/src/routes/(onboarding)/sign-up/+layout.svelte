bfb1905efa9c8a11f8059d639128aa8b6610381d
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-22 22:24:47 +0100

    chore(web): add header to sign-up layout

--- apps/web/src/routes/(onboarding)/sign-up/+layout.svelte
+++ apps/web/src/routes/(onboarding)/sign-up/+layout.svelte
@@ -1,1 +1,25 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	onMount(async () => {
+    await import('ui');
+  });
+</script>
+
+<header>
+	<a href="/">
+		<fd-logo />
+	</a>
+</header>
 <slot></slot>
+
+<style lang="postcss">
+  header {
+    display: grid;
+		place-items: center;
+		padding-block: var(--size-2);
+
+    @media (--tablet) {
+      padding-block: var(--size-3);
+    }
+  }
+</style>
