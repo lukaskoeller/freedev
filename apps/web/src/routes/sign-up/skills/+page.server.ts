d00fc2c3abc19697ef357899c6e975a26193d660
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-24 00:56:18 +0100

    feat(web): add skills page

---
+++ apps/web/src/routes/sign-up/skills/+page.server.ts
@@ -0,0 +1,8 @@
+import { redirect, type Actions } from "@sveltejs/kit";
+
+export const actions: Actions = {
+  default: async ({ request, fetch }) => {
+    // @todo TEMPORARY Remove
+    throw redirect(301, '/profile');
+  },
+};
---
+++ apps/web/src/routes/sign-up/skills/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import FormContainer from "../FormContainer.svelte";
+
+  onMount(async () => {
+    await import('ui');
+  });
+
+  const heading: string = 'What do you rock at?';
+</script>
+
+<svelte:head>
+	<title>Sign Up / {heading}</title>
+</svelte:head>
+
+<FormContainer heading={heading}>
+  <form
+    id="sign-up-skills"
+    method="POST"
+  >
+    <div class="fd-stack">
+      <fd-input
+        name="programmingLanguages"
+        type="text"
+        label="Languages"
+        required
+      ></fd-input>
+      <fd-input
+        name="stack"
+        type="text"
+        label="Stack"
+        required
+      ></fd-input>
+      <fd-input
+        name="tools"
+        type="text"
+        label="Tools"
+        required
+      ></fd-input>
+    </div> 
+  </form>
+  <fd-button
+    slot="footer"
+    form="sign-up-skills"
+    expand
+  >
+    Go to profile
+  </fd-button>
+</FormContainer>
