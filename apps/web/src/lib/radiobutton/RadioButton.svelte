87a7a2ced72d1eb4849620dca26768f1bbb935bd
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-25 15:28:02 +0100

    feat(web): add RadioButton component

---
+++ apps/web/src/lib/radiobutton/RadioButton.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let name: string;
+  export let id: string;
+  export let value: string;
+  export let label: string;
+</script>
+
+<label for={id} class="fd-radio-button">
+  <input
+    type="radio"
+    {name}
+    {id}
+    {value}
+  >
+  <div>{label}</div>
+</label>
