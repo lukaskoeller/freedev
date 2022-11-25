3e3d4933f6d23182fea92426705ce9252b25b674
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-25 15:27:51 +0100

    feat(web): add CheckboxButton component

---
+++ apps/web/src/lib/checkboxbutton/CheckboxButton.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  export let id: string;
+  export let name: string;
+  export let label: string;
+</script>
+
+<label for={id} class="fd-checkbox-button">
+  <input
+    type="checkbox"
+    {name}
+    {id}
+  >
+  <div>{label}</div>
+</label>
