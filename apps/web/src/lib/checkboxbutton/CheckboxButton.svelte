cfa0b528937a45a28ffdbef784e17d46a3184904
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-26 22:52:47 +0100

    chore(web): use slots only

--- apps/web/src/lib/checkboxbutton/CheckboxButton.svelte
+++ apps/web/src/lib/checkboxbutton/CheckboxButton.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
   export let id: string;
-  export let name: string;
-  export let label: string;
 </script>
 
 <label for={id} class="fd-checkbox-button">
-  <input
-    type="checkbox"
-    {name}
-    {id}
-  >
-  <div>{label}</div>
+  <slot></slot>
+  <div class="label">
+    <slot name="label"></slot>
+  </div>
 </label>
