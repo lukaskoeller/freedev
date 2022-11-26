ae9e610f0d238de563546a23b79e4ac1b88f143c
Lukas Koeller <acc.koeller@protonmail.com>
2022-11-26 22:53:44 +0100

    chore(web): use slots only in RadioButton

--- apps/web/src/lib/radiobutton/RadioButton.svelte
+++ apps/web/src/lib/radiobutton/RadioButton.svelte
@@ -1,16 +1,8 @@
 <script lang="ts">
-  export let name: string;
-  export let id: string;
-  export let value: string;
-  export let label: string;
+  export let id: string = undefined;
 </script>
 
 <label for={id} class="fd-radio-button">
-  <input
-    type="radio"
-    {name}
-    {id}
-    {value}
-  >
-  <div>{label}</div>
+  <slot></slot>
+  <div><slot name="label"></slot></div>
 </label>
