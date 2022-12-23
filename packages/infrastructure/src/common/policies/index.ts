99abb6e0912fff6fa8929989fd2541932102693f
Lukas Koeller <acc.koeller@protonmail.com>
2022-12-23 22:51:43 +0100

    feat: add iam-policy-write-dynamodb policy

--- packages/infrastructure/src/common/policies/index.ts
+++ packages/infrastructure/src/common/policies/index.ts
@@ -1,6 +1,47 @@
 import * as aws from "@pulumi/aws";
 import { DYNAMO_DB_TABLE_ARN } from "../../../../functions/src/common/constants";
 
+export const policyWriteDynamodb = new aws.iam.Policy("iam-policy-write-dynamodb", {
+  path: "/",
+  description: "Provides write access to DynamoDB and DynamoDB Streams",
+  policy: {
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+          "Sid": "ReadWriteTable",
+          "Effect": "Allow",
+          "Action": [
+              "dynamodb:BatchWriteItem",
+              "dynamodb:PutItem",
+              "dynamodb:UpdateItem"
+          ],
+          "Resource": DYNAMO_DB_TABLE_ARN,
+      },
+      {
+          "Sid": "GetStreamRecords",
+          "Effect": "Allow",
+          "Action": "dynamodb:GetRecords",
+          "Resource": DYNAMO_DB_TABLE_ARN,
+      },
+      {
+          "Sid": "WriteLogStreamsAndGroups",
+          "Effect": "Allow",
+          "Action": [
+              "logs:CreateLogStream",
+              "logs:PutLogEvents"
+          ],
+          "Resource": DYNAMO_DB_TABLE_ARN,
+      },
+      {
+          "Sid": "CreateLogGroup",
+          "Effect": "Allow",
+          "Action": "logs:CreateLogGroup",
+          "Resource": DYNAMO_DB_TABLE_ARN,
+      }
+    ]
+  },
+});
+
 export const policyReadWriteDynamodb = new aws.iam.Policy("iam-policy-read-write-dynamodb", {
   path: "/",
   description: "Provides read and write (excluding scan) access to DynamoDB and DynamoDB Streams",
