381f0e9423c8ccf5cc7fc6ac91dd27e61638dfe8
Lukas Koeller <acc.koeller@protonmail.com>
2022-12-23 22:47:08 +0100

    refactor: use fn from lib for signin server handling

--- apps/web/src/routes/(app)/sign-in/+page.server.ts
+++ apps/web/src/routes/(app)/sign-in/+page.server.ts
@@ -1,69 +1,9 @@
-import { api } from "$lib/common/utils/api.utils";
-import { dev } from '$app/environment';
-import { invalid, redirect, type Actions } from "@sveltejs/kit";
-
-export type NewDeviceMetadata = {
-  DeviceGroupKey: string;
-  DeviceKey: string;
-}
-
-export type AuthenticationResult = {
-  AccessToken: string;
-  ExpiresIn: number;
-  IdToken: string;
-  NewDeviceMetadata: NewDeviceMetadata,
-  RefreshToken: string;
-  TokenType: string;
-};
+import { redirect, type Actions } from "@sveltejs/kit";
+import { signInServer } from "$lib/signinform/signIn.server";
 
 export const actions: Actions = {
-  default: async ({ request, cookies, fetch }) => {
-    const form = await request.formData();
-    console.log('FORM', form);
-
-    const email = form.get('email');
-    const password = form.get('password');
-  
-    const response = await api({
-      fetch,
-      method: 'POST',
-      resource: 'sign-in',
-      data: {
-        email,
-        password,
-      },
-    });
-    const body = await response.json();
-
-    console.log({
-      response,
-      body,
-    });
-
-    if (body?.statusCode === 500) {
-      return invalid(500, { message: body?.message });
-    }
-
-    if (body?.statusCode === 400) {
-      return invalid(400, { message: body?.message });
-    }
-
-    const authResult = body.AuthenticationResult as AuthenticationResult;
-
-    cookies.set('token', authResult.AccessToken, {
-      // send cookie for every page
-      path: '/',
-      // server side only cookie so you can't use `document.cookie`
-      httpOnly: true,
-      // only requests from same site can send cookies
-      // https://developer.mozilla.org/en-US/docs/Glossary/CSRF
-      sameSite: 'strict',
-      // only sent over HTTPS in production
-      // @todo use different variable? see https://vitejs.dev/guide/env-and-mode.html
-      secure: !dev,
-      // set cookie to expire after a month
-      maxAge: authResult.ExpiresIn,
-    })
+  default: async ({ request, cookies }) => {
+    await signInServer(request, cookies);
 
     throw redirect(301, '/profile');
   },
