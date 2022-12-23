fe4386dbc1ba1898311de9ae1d329ca32ffe1493
Lukas Koeller <acc.koeller@protonmail.com>
2022-12-23 22:45:26 +0100

    feat: add InputGroup component

---
+++ apps/web/src/lib/inputgroup/InputGroup.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  export let text: string = '';
+</script>
+
+<div>
+  <div class="fd-input-group">
+    <slot></slot>
+  </div>
+  {#if $$slots.text || text}
+    <div
+      class="fd-error fd-caption"
+      role="alert"
+    >
+      <slot name="text">
+        {text}
+      </slot>
+    </div>
+  {/if}
+</div>
