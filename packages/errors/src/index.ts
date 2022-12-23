91f3f48dd3bd856a023e0b2a48a2de627c5c5cb9
Lukas Koeller <acc.koeller@protonmail.com>
2022-12-23 22:50:40 +0100

    feat: add error msg for only numbers allowed

--- packages/errors/src/index.ts
+++ packages/errors/src/index.ts
@@ -1,3 +1,4 @@
 export const DEFAULT_ERROR_MESSAGE = 'An error occured. Please try again later.';
 export const REQUIRED_ERROR_MESSAGE = 'This field is required.';
 export const INTERNAL_ERROR_MESSAGE = 'An internal error occured on our side. We could not sign you in. Please try connecting again. If the issue keeps happening, contact Customer Care.';
+export const ONLY_NUMBER_ERROR_MESSAGE = 'The field should only contain numbers.';
