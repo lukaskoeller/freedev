379b59fec723ffffddb998c6b1ab6ff1fa7706e3
Lukas Koeller <acc.koeller@protonmail.com>
2023-01-09 23:00:30 +0100

    chore(functions): mark constants as deprecated

--- packages/functions/src/common/constants.ts
+++ packages/functions/src/common/constants.ts
@@ -1,26 +1,38 @@
 /**
  * AWS Region
  * @see https://www.pulumi.com/registry/packages/aws/api-docs/getregion/
+ * @deprecated use package `@freedev/constants` instead`
  */
 export const AWS_REGION = 'eu-central-1';
 /**
  * Cognito User Pool Client ID
+ * Copy saved in `web/.env`.
+ * @deprecated use package `@freedev/constants` instead`
  */
-export const USER_POOL_CLIENT_ID = '6b3h1osi597ifm5at8qcsavg5d';
+export const USER_POOL_CLIENT_ID = '43em4pbfrtrng5cvjriuq5gd96';
 /**
  * Cognito User Pool ID
+ * @deprecated use package `@freedev/constants` instead`
  */
 export const USER_POOL_ID = 'eu-central-1_RhkLD4h08';
 /**
  * Main DynamoDB Table Name
+ * @deprecated use package `@freedev/constants` instead`
  */
 export const DYNAMO_DB_TABLE_NAME = 'main-dynamodb-table-bfad842';
+/**
+ * @deprecated use package `@freedev/constants` instead`
+ */
 export const DYNAMO_DB_TABLE_NAME_INDEX = 'handle-index';
+/**
+ * @deprecated use package `@freedev/constants` instead`
+ */
 export const DYNAMO_DB_TABLE_ARN = 'arn:aws:dynamodb:eu-central-1:865039251033:table/main-dynamodb-table-bfad842';
 
 /**
  * DynamoDB Prefixes to determine data type
  * in a single table application.
+ * @deprecated use package `@freedev/constants` instead`
  */
 export enum DBKeyPrefix {
   User = 'USER#',
