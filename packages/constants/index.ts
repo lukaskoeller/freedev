e011202472a4978acbdf913a0eb9d80cb5847259
Lukas Koeller <acc.koeller@protonmail.com>
2023-01-09 22:58:40 +0100

    feat(web): add constants package

---
+++ packages/constants/index.ts
@@ -0,0 +1,27 @@
+/**
+ * AWS Region
+ * @see https://www.pulumi.com/registry/packages/aws/api-docs/getregion/
+ */
+export const AWS_REGION = 'eu-central-1';
+/**
+ * Cognito User Pool Client ID
+ */
+export const USER_POOL_CLIENT_ID = '43em4pbfrtrng5cvjriuq5gd96';
+/**
+ * Cognito User Pool ID
+ */
+export const USER_POOL_ID = 'eu-central-1_RhkLD4h08';
+/**
+ * Main DynamoDB Table Name
+ */
+export const DYNAMO_DB_TABLE_NAME = 'main-dynamodb-table-bfad842';
+export const DYNAMO_DB_TABLE_NAME_INDEX = 'handle-index';
+export const DYNAMO_DB_TABLE_ARN = 'arn:aws:dynamodb:eu-central-1:865039251033:table/main-dynamodb-table-bfad842';
+
+/**
+ * DynamoDB Prefixes to determine data type
+ * in a single table application.
+ */
+export enum DBKeyPrefix {
+  User = 'USER#',
+}
