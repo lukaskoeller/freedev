e3d8aa9a0ed630e396442c0ab1f9f4fea1a6c6ac
Lukas Koeller <acc.koeller@protonmail.com>
2023-02-02 00:02:46 +0100

    chore(web): rm unnecessary code

--- apps/web/src/routes/(app)/profile/+page.server.ts
+++ apps/web/src/routes/(app)/profile/+page.server.ts
@@ -1,17 +1,12 @@
 import { api } from '$lib/common/utils/api.utils';
 import { getAccessToken } from '$lib/common/utils/auth.utils';
 import type { PageServerLoad } from './$types';
-// import { getToken } from '@auth/core/jwt';
 
 export const load: PageServerLoad = async ({ cookies, request, fetch }) => {
 
   const { accessToken: token } = await getAccessToken(cookies);
-  // @see https://github.com/nextauthjs/next-auth/discussions/5595#discussioncomment-4651584
-  // @see https://authjs.dev/guides/basics/securing-pages-and-api-routes#using-gettoken
-  // const token = await getToken({
-  //   req: request,
-  //   secret: COGNITO_CLIENT_SECRET,
-  // })
+  console.log({ token });
+  
   
   const response = await api({
     fetch,
@@ -20,7 +15,7 @@ export const load: PageServerLoad = async ({ cookies, request, fetch }) => {
     token,
   });
   const body = await response.json();
-  
+  console.log({ body });
 
   return body;
 }
