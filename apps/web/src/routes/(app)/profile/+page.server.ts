efa514eb762c1552bb40c1c90ed8392200177179
Lukas Koeller <acc.koeller@protonmail.com>
2023-02-01 08:42:10 +0100

    chore(web): add accessToken to api request

--- apps/web/src/routes/(app)/profile/+page.server.ts
+++ apps/web/src/routes/(app)/profile/+page.server.ts
@@ -1,9 +1,17 @@
 import { api } from '$lib/common/utils/api.utils';
+import { getAccessToken } from '$lib/common/utils/auth.utils';
 import type { PageServerLoad } from './$types';
+// import { getToken } from '@auth/core/jwt';
 
 export const load: PageServerLoad = async ({ cookies, request, fetch }) => {
-  console.log({ request });
-  const token = cookies.get('token');
+
+  const { accessToken: token } = await getAccessToken(cookies);
+  // @see https://github.com/nextauthjs/next-auth/discussions/5595#discussioncomment-4651584
+  // @see https://authjs.dev/guides/basics/securing-pages-and-api-routes#using-gettoken
+  // const token = await getToken({
+  //   req: request,
+  //   secret: COGNITO_CLIENT_SECRET,
+  // })
   
   const response = await api({
     fetch,
@@ -12,6 +20,7 @@ export const load: PageServerLoad = async ({ cookies, request, fetch }) => {
     token,
   });
   const body = await response.json();
+  
 
   return body;
 }
