4693e62a5f687e975e0ed3f04bcc4286fef1d84f
Lukas Koeller <acc.koeller@protonmail.com>
2023-02-02 00:02:05 +0100

    docs(web): add helpful comments to auth.utils

--- apps/web/src/lib/common/utils/auth.utils.ts
+++ apps/web/src/lib/common/utils/auth.utils.ts
@@ -3,6 +3,11 @@ import { AUTH_SECRET } from "$env/static/private";
 import { TOKEN_NAME } from "../constants";
 import type { Cookies } from "@sveltejs/kit";
 
+/**
+ * @see https://authjs.dev/reference/sveltekit/modules/main#usage
+ * @see https://github.com/nextauthjs/next-auth/discussions/5595#discussioncomment-4651584
+ * @see https://github.com/xmlking/svelte-starter-kit/blob/main/src/lib/server/middleware/authjs-helper.ts
+ */
 export const getAccessToken = async (cookies: Cookies, raw = false) => {
   const token = cookies.get(TOKEN_NAME); // TOKEN_NAME = "next-auth.session-token"
   if (raw) return token;
@@ -11,3 +16,12 @@ export const getAccessToken = async (cookies: Cookies, raw = false) => {
     secret: AUTH_SECRET,
   });
 };
+
+// @todo
+// @see https://github.com/nextauthjs/next-auth/discussions/5595#discussioncomment-4651584
+// @see https://authjs.dev/guides/basics/securing-pages-and-api-routes#using-gettoken
+// import { getToken } from '@auth/core/jwt';
+// const token = await getToken({
+//   req: request,
+//   secret: COGNITO_CLIENT_SECRET,
+// })
