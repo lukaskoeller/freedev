6777284e9ec32dcee4a63d880e35a2f05b05868f
Lukas Koeller <acc.koeller@protonmail.com>
2023-02-09 00:40:34 +0100

    feat(web): add InputWrapper component

---
+++ apps/web/src/lib/inputwrapper/InputWrapper.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  export let id: string;
+  export let text: string = '';
+</script>
+
+<div class="container">
+  <label class="fd-label" for={id}>
+    <slot name="label"></slot>
+  </label>
+  <slot></slot>
+  <div
+    class="fd-error fd-caption"
+    role="alert"
+    id="error"
+  >
+    <slot name="error">
+      {text}
+    </slot>
+  </div>
+</div>
+
+<style lang="postcss">
+  .container {
+    display: grid;
+    row-gap: calc(var(--size-1) / 2);
+  }
+</style>
