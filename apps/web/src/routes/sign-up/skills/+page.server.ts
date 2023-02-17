e384f9e235b1ff7fef2af563349131fc07503219
Lukas Koeller <acc.koeller@protonmail.com>
2023-02-18 00:47:46 +0100

    chore(web): grap form data of skills form

--- apps/web/src/routes/sign-up/skills/+page.server.ts
+++ apps/web/src/routes/sign-up/skills/+page.server.ts
@@ -1,8 +1,25 @@
+import { getAccessToken } from "$lib/common/utils/auth.utils";
 import { redirect, type Actions } from "@sveltejs/kit";
 
 export const actions: Actions = {
-  default: async ({ request, fetch }) => {
+  default: async ({ request, cookies, fetch }) => {
+    const { accessToken: token } = await getAccessToken(cookies);
+    const form = await request.formData();
+    console.log(form);
+
+    const languages = form.getAll('languages');
+    const technologies = form.getAll('technologies');
+    const tools = form.getAll('tools');
+
+    const data = {
+      languages,
+      technologies,
+      tools,
+    };
+
+    console.log(data);
+
     // @todo TEMPORARY Remove
-    throw redirect(301, '/profile');
+    // throw redirect(301, '/profile');
   },
 };
