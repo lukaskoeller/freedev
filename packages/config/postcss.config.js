9298290d645fc7c8ea0406bd6a46c7158f6c3a36
Lukas Koeller <acc.koeller@protonmail.com>
2023-03-24 09:09:44 +0100

    chore(config): get rid of postcss importFrom

--- packages/config/postcss.config.js
+++ packages/config/postcss.config.js
@@ -13,14 +13,6 @@ module.exports = {
     }),
     postcssPresetEnv({
       stage: 1,
-      importFrom: [
-        { customMedia: { '--mobile': '(min-width: 320px)' } },
-        { customMedia: { '--large-mobile': '(min-width: 480px)' } },
-        { customMedia: { '--tablet': '(min-width: 768px)' } },
-        { customMedia: { '--small-desktop': '(min-width: 1024px)' } },
-        { customMedia: { '--large-desktop': '(min-width: 1312px)' } },
-        { customSelectors: { ':--heading': 'h1, h2, h3, h4, h5, h6' } },
-      ],
       features: {
         'logical-properties-and-values': {
           dir: 'ltr'
