8a88d5b4d10f5d7a5b88846f1953ecb1f17d3793
Lukas Koeller <acc.koeller@protonmail.com>
2023-04-10 08:12:29 +0200

    chore(web): comment signin server due to authjs

--- apps/web/src/routes/(app)/sign-in/+page.server.ts
+++ apps/web/src/routes/(app)/sign-in/+page.server.ts
@@ -1,10 +1,11 @@
 import { redirect, type Actions } from "@sveltejs/kit";
+import { signIn } from "@auth/sveltekit/client";
 import { signInServer } from "$lib/signinform/signIn.server";
 
 export const actions: Actions = {
   default: async ({ request, cookies }) => {
-    await signInServer(request, cookies);
+    // await signInServer(request, cookies);
 
-    throw redirect(301, '/profile');
+    // throw redirect(301, '/profile');
   },
 };
