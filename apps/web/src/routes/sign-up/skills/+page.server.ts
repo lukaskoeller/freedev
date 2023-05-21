09095ef7996bf8373372211502ac3590751fc99b
Lukas Koeller <acc.koeller@protonmail.com>
2023-05-21 22:38:28 +0200

    chore(web): prepare skills data

--- apps/web/src/routes/sign-up/skills/+page.server.ts
+++ apps/web/src/routes/sign-up/skills/+page.server.ts
@@ -1,12 +1,12 @@
 import { api } from "$lib/common/utils/api.utils";
 import { getAccessToken } from "$lib/common/utils/auth.utils";
+import { ALL_SKILLS_MAP } from "$lib/constants/skills";
 import { redirect, type Actions } from "@sveltejs/kit";
 
 export const actions: Actions = {
   default: async ({ request, cookies, fetch }) => {
     const { accessToken: token } = await getAccessToken(cookies);
     const form = await request.formData();
-    console.log(form);
 
     const language = form.getAll('languages');
     const technology = form.getAll('technologies');
@@ -18,25 +18,24 @@ export const actions: Actions = {
       tool,
     };
 
-    const data = Object.entries(rawData).flatMap(([key, value]) => {
-      return Array.isArray(value)
-        ? value.map((val: string) => ({
-          category: key,
-          skill: val,
-        })) : ({
-          category: key,
-          skill: value,
-        })
-    });
+    
+    const data = Object.values(rawData).flat().map((item) => ALL_SKILLS_MAP[item as string]);
+    
+    const cleanData = data.map(({ name, category, subcategory }) => ({
+      name,
+      category,
+      subcategory,
+    }));
 
-    console.log(data);
     const response = await api({
       fetch,
       method: 'PUT',
       resource: 'user/skills',
-      data,
+      data: cleanData,
       token,
     });
+
+    console.log(response);
 
     if (response.status === 200) {
       throw redirect(301, '/profile');
